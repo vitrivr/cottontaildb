c5c2bcdc669183d776e0763115aa5bf3fef8568f
Ralph Gasser <rg@pontius.ch>
2019-01-20 10:06:52 +0100

    Changed signature of KnnContainer#add()

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/knn/KnnContainer.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/knn/KnnContainer.kt
@@ -10,20 +10,20 @@ import java.util.*
 class KnnContainer(private val n: Int, private val query: FloatArray, private val distance: DistanceFunction) {
 
     /**  */
-    val knn = TreeSet<Pair<Long, Float>> { o1, o2 -> o1.second.compareTo(o2.second) }
+    val knn = TreeSet<Pair<Long,Double>> { o1, o2 -> o1.second.compareTo(o2.second) }
 
     /**
      *
      * @param id
      * @param vector
      */
-    fun add(id: Long, vector: FloatArray) {
-        if (vector.size != this.query.size) throw IllegalArgumentException("The query vector and the provided vector do not have the same size.")
-        val dist = this.distance(this.query, vector)
+    fun add(tupleId: Long, feature: FloatArray) {
+        if (feature.size != this.query.size) throw IllegalArgumentException("The query vector and the provided vector do not have the same size.")
+        val dist = this.distance(this.query, feature)
         if (this.knn.size < this.n) {
-            this.knn.add(Pair(id, dist))
+            this.knn.add(Pair(tupleId, dist))
         } else if (dist < this.knn.last().second) {
-            this.knn.add(Pair(id, dist))
+            this.knn.add(Pair(tupleId, dist))
             this.knn.pollLast()
         }
     }
@@ -32,7 +32,7 @@ class KnnContainer(private val n: Int, private val query: FloatArray, private va
      *
      * @return
      */
-    fun knn(): List<Pair<Long, Float>> {
+    fun knn(): List<Pair<Long, Double>> {
         return this.knn.toList()
     }
 }
