7e9ba47b11bbcdf5f7a2abb22fd2632e15dae769
Ralph Gasser <rg@pontius.ch>
2019-01-22 00:27:10 +0100

    Optimized existing distance functions.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/knn/metrics/Distance.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/knn/metrics/Distance.kt
@@ -1,38 +1,62 @@
 package ch.unibas.dmi.dbis.cottontail.knn.metrics
-enum class Distance : DistanceFunction {
 
+enum class Distance : DistanceFunction {
+    /**
+     * L1 or Manhattan distance between two vectorss
+     */
     L1 {
         override fun invoke(a: FloatArray, b: FloatArray): Double {
-            var dist = 0.0
-            for (i in a.indices) {
-                dist += StrictMath.abs(b[i] - a[i])
+            for (i in 0 until b.size) {
+                b[i] = b[i] - a[i]
+                if (b[i] < 0.0) b[i] = 0.0f-b[i]
             }
-            return dist
+            return b.sum().toDouble()
         }
 
         override fun invoke(a: DoubleArray, b: DoubleArray): Double {
-            var dist = 0.0
-            for (i in a.indices) {
-                dist += StrictMath.abs(b[i] - a[i])
+            for (i in 0 until b.size) {
+                b[i] = b[i] - a[i]
+                if (b[i] < 0.0) b[i] = 0.0-b[i]
             }
-            return dist
+            return b.sum()
         }
     },
+
+    /**
+     * L2 or Euclidian distance between two vectors. Vectors must be of the same size!
+     */
     L2 {
         override fun invoke(a: FloatArray, b: FloatArray): Double {
-            var dist = 0.0
-            for (i in a.indices) {
-                dist += StrictMath.pow((b[i] - a[i]).toDouble(), 2.0)
+            for (i in 0 until b.size) {
+                b[i] = (b[i] - a[i]) * (b[i] - a[i])
             }
-            return Math.sqrt(dist)
+            return StrictMath.sqrt(b.sum().toDouble())
         }
 
         override fun invoke(a: DoubleArray, b: DoubleArray): Double {
-            var dist = 0.0
-            for (i in a.indices) {
-                dist += StrictMath.pow((b[i] - a[i]), 2.0)
+            for (i in 0 until b.size) {
+                b[i] = (b[i] - a[i]) * (b[i] - a[i])
             }
-            return Math.sqrt(dist)
+            return StrictMath.sqrt(b.sum())
+        }
+    },
+
+    /**
+     * Squared L2 or Euclidian distance between two vectors. Vectors must be of the same size!
+     */
+    L2SQUARED {
+        override fun invoke(a: FloatArray, b: FloatArray): Double {
+            for (i in 0 until b.size) {
+                b[i] = (b[i] - a[i]) * (b[i] - a[i])
+            }
+            return b.sum().toDouble()
+        }
+
+        override fun invoke(a: DoubleArray, b: DoubleArray): Double {
+            for (i in 0 until b.size) {
+                b[i] = (b[i] - a[i]) * (b[i] - a[i])
+            }
+            return b.sum()
         }
     }
 }
