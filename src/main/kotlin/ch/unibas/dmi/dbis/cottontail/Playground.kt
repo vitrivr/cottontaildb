783a097fcf3d1aafbea682ec476a02c9dc67013a
Ralph Gasser <rg@pontius.ch>
2019-01-24 06:42:13 +0100

    Updated playground.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/Playground.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/Playground.kt
@@ -2,13 +2,11 @@ package ch.unibas.dmi.dbis.cottontail
 
 import ch.unibas.dmi.dbis.cottontail.config.Config
 import ch.unibas.dmi.dbis.cottontail.database.schema.*
-import ch.unibas.dmi.dbis.cottontail.knn.FloatVectorRow
-import ch.unibas.dmi.dbis.cottontail.knn.FloatVectorRowSerializer
 import ch.unibas.dmi.dbis.cottontail.knn.KnnContainer
 import ch.unibas.dmi.dbis.cottontail.knn.metrics.Distance
-import ch.unibas.dmi.dbis.cottontail.model.DatabaseException
+
 import com.google.gson.GsonBuilder
-import org.db.mapdb.list.LinkedListFactory
+import kotlinx.serialization.json.JSON
 
 import java.io.IOException
 import java.nio.file.Files
@@ -22,22 +20,20 @@ object Playground {
     fun main(args: Array<String>) {
         val path = args[0]
         Files.newBufferedReader(Paths.get(path)).use { reader ->
-            val gson = GsonBuilder().create()
-            val config = gson.fromJson(reader, Config::class.java)
-            if (config != null) {
+            val config = JSON.parse(Config.serializer(), reader.readText())
                 loadAndPersist(config)
-            }
         }
     }
 
     fun loadAndRead(config: Config) {
         try {
-            val entity = Entity("test", Paths.get(config.dataFolder))
+            val schema = Schema("adampro", config)
+            val entity = schema.get("test")
             val tx = entity.Tx(readonly = true)
 
             val block = {
                 val start = System.currentTimeMillis()
-                val container = KnnContainer(10000, FloatArray(512), Distance.L2)
+                val container = KnnContainer(10000,FloatArray(512), Distance.L1)
 
                 tx.forEachColumn({ l: Long, floats: FloatArray -> container.add(l,floats) }, ColumnType.specForName("feature","DFARRAY"))
                 println(String.format("%s: kNN for n=%d, s=%d vectors took %d ms", Thread.currentThread().name, container.knn.size, tx.count(), System.currentTimeMillis() - start))
@@ -56,13 +52,13 @@ object Playground {
     }
 
 
-    @Throws(IOException::class, DatabaseException::class)
     fun loadAndPersist(config: Config) {
 
 
-        val entity = Entity.initialize("test", Paths.get(config.dataFolder), ColumnDef("id", StringColumnType()), ColumnDef("feature", FloatArrayColumnType()))
+        val schema = Schema.create("adampro", config)
+        val entity = schema.createEntity("test", ColumnDef("id", StringColumnType()), ColumnDef("feature", FloatArrayColumnType()))
         val tx = entity.Tx(readonly = false)
-        tx.execute {
+        tx.begin {
             for (i in 1..76) {
                 Files.newBufferedReader(Paths.get(String.format("/Volumes/Data (Mac)/Extracted/features_surfmf25k512_%d.json", i))).use { reader ->
                     val gson = GsonBuilder().create()
