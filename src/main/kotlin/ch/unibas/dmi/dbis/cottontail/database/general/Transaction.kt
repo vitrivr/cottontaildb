ebdeb9bf503976200898dd843f9d0955f992b632
Ralph Gasser <rg@pontius.ch>
2019-01-25 17:48:26 +0100

    Transactions now work as intended thanks to inline functions.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/database/general/Transaction.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/database/general/Transaction.kt
@@ -6,31 +6,19 @@ import java.util.*
 
 /**
  * This is a [Transaction] object as used by different [DBO] objects to facilitate changes.
+ *
+ * @author Ralph Gasser
+ * @version 1.0f
  */
 interface Transaction : AutoCloseable {
     /** [TransactionStatus] of this [Transaction]. */
     val status: TransactionStatus
 
-    /** Inidactor whether this [Transaction] is readonly. */
+    /** Indicator whether this [Transaction] is readonly. */
     val readonly: Boolean
 
     /** The UUID that identifies this [Transaction]. */
     val tid: UUID
-
-    /**
-     *
-     */
-    fun begin(action: () -> Boolean) = try {
-        if (action()) {
-            commit()
-        } else {
-            rollback()
-        }
-    } catch (e: Exception) {
-        rollback()
-    } finally {
-        close()
-    }
 
     /**
      * Commits the transaction, saving all changes. Causes the [Transaction] to complete and close.
@@ -41,4 +29,26 @@ interface Transaction : AutoCloseable {
      * Rolls the [Transaction], causing changes to be lost. Causes the [Transaction] to complete and close.
      */
     fun rollback()
+}
+
+/**
+ * An inline function that can be used to create a transactional context from a [Transaction].
+ *
+ * The provided block will be executed as a [Transaction] and any exception thrown in the block will result
+ * in a rollback. Once the block has been executed successfully, the [Transaction] is committed.
+ *
+ * In both cases, the [Transaction] that has been used will be closed.
+ *
+ * @param block The block that should be executed in a [Transaction] context.
+ */
+fun <T:Transaction> T.begin(block: (tx: T) -> Boolean) = try {
+    if (block(this)) {
+        commit()
+    } else {
+        rollback()
+    }
+} catch (e: Exception) {
+    rollback()
+} finally {
+    close()
 }
