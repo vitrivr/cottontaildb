0bf6c57ce53386bc4999ac8cdf4cffb712aff96b
Luca Rossetto <lucaro@lucaro.ch>
2019-01-30 22:28:16 +0100

    Added weighted versions of distance functions

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/knn/metrics/Distance.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/knn/metrics/Distance.kt
@@ -5,6 +5,24 @@ enum class Distance : DistanceFunction {
      * L1 or Manhattan distance between two vectors. Vectors must be of the same size!
      */
     L1 {
+        override fun invoke(a: FloatArray, b: FloatArray, weights: FloatArray): Double {
+            for (i in 0 until b.size) {
+                b[i] = b[i] - a[i]
+                if (b[i] < 0.0) b[i] = 0.0f-b[i]
+                b[i] = b[i] * weights[i]
+            }
+            return b.sum().toDouble()
+        }
+
+        override fun invoke(a: DoubleArray, b: DoubleArray, weights: DoubleArray): Double {
+            for (i in 0 until b.size) {
+                b[i] = b[i] - a[i]
+                if (b[i] < 0.0) b[i] = 0.0-b[i]
+                b[i] = b[i] * weights[i]
+            }
+            return b.sum()
+        }
+
         override fun invoke(a: FloatArray, b: FloatArray): Double {
             for (i in 0 until b.size) {
                 b[i] = b[i] - a[i]
@@ -26,6 +44,20 @@ enum class Distance : DistanceFunction {
      * L2 or Euclidian distance between two vectors. Vectors must be of the same size!
      */
     L2 {
+        override fun invoke(a: FloatArray, b: FloatArray, weights: FloatArray): Double {
+            for (i in 0 until b.size) {
+                b[i] = (b[i] - a[i]) * (b[i] - a[i]) * weights[i]
+            }
+            return StrictMath.sqrt(b.sum().toDouble())
+        }
+
+        override fun invoke(a: DoubleArray, b: DoubleArray, weights: DoubleArray): Double {
+            for (i in 0 until b.size) {
+                b[i] = (b[i] - a[i]) * (b[i] - a[i]) * weights[i]
+            }
+            return StrictMath.sqrt(b.sum())
+        }
+
         override fun invoke(a: FloatArray, b: FloatArray): Double {
             for (i in 0 until b.size) {
                 b[i] = (b[i] - a[i]) * (b[i] - a[i])
@@ -45,6 +77,20 @@ enum class Distance : DistanceFunction {
      * Squared L2 or Euclidian distance between two vectors. Vectors must be of the same size!
      */
     L2SQUARED {
+        override fun invoke(a: FloatArray, b: FloatArray, weights: FloatArray): Double {
+            for (i in 0 until b.size) {
+                b[i] = (b[i] - a[i]) * (b[i] - a[i]) * weights[i]
+            }
+            return b.sum().toDouble()
+        }
+
+        override fun invoke(a: DoubleArray, b: DoubleArray, weights: DoubleArray): Double {
+            for (i in 0 until b.size) {
+                b[i] = (b[i] - a[i]) * (b[i] - a[i]) * weights[i]
+            }
+            return b.sum()
+        }
+
         override fun invoke(a: FloatArray, b: FloatArray): Double {
             for (i in 0 until b.size) {
                 b[i] = (b[i] - a[i]) * (b[i] - a[i])
--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/knn/metrics/DistanceFunction.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/knn/metrics/DistanceFunction.kt
@@ -6,8 +6,13 @@ interface DistanceFunction {
      */
     operator fun invoke(a: FloatArray, b: FloatArray): Double
 
+    operator fun invoke(a: FloatArray, b: FloatArray, weights: FloatArray): Double
+
     /**
      *
      */
     operator fun invoke(a: DoubleArray, b: DoubleArray): Double
+
+    operator fun invoke(a: DoubleArray, b: DoubleArray, weights: DoubleArray): Double
+
 }
