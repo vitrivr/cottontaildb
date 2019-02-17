56f8b64c35e66f3689e75be447fca55f2048967f
Ralph Gasser <rg@pontius.ch>
2019-02-17 18:36:20 +0100

    Commited example of using GRPC endpoint.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/Playground.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/Playground.kt
@@ -1,14 +1,15 @@
 package ch.unibas.dmi.dbis.cottontail
 
-import ch.unibas.dmi.dbis.cottontail.config.Config
-import ch.unibas.dmi.dbis.cottontail.database.catalogue.Catalogue
-import ch.unibas.dmi.dbis.cottontail.model.basics.ColumnDef
-import ch.unibas.dmi.dbis.cottontail.database.general.begin
-import ch.unibas.dmi.dbis.cottontail.model.basics.StandaloneRecord
+import ch.unibas.dmi.dbis.cottontail.grpc.CottonDDLGrpc
+import ch.unibas.dmi.dbis.cottontail.grpc.CottonDMLGrpc
+import ch.unibas.dmi.dbis.cottontail.grpc.CottonDQLGrpc
+import ch.unibas.dmi.dbis.cottontail.grpc.CottontailGrpc
 import ch.unibas.dmi.dbis.cottontail.model.exceptions.DatabaseException
+import ch.unibas.dmi.dbis.cottontail.server.grpc.helper.DataHelper
+import ch.unibas.dmi.dbis.cottontail.utilities.VectorUtility
 
 import com.google.gson.GsonBuilder
-import kotlinx.serialization.json.JSON
+import io.grpc.ManagedChannelBuilder
 
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -16,62 +17,85 @@ import java.nio.file.Paths
 
 object Playground {
 
+
+    val channel = ManagedChannelBuilder.forAddress("127.0.0.1", 1865).usePlaintext().build()
+
+    val dqlService =  CottonDQLGrpc.newBlockingStub(channel)
+    val ddlService =  CottonDDLGrpc.newBlockingStub(channel)
+    val dmlService =  CottonDMLGrpc.newBlockingStub(channel)
+
+    val schema = CottontailGrpc.Schema.newBuilder().setName("test").build()
+    val entity = CottontailGrpc.Entity.newBuilder()
+            .setSchema(schema)
+            .setName("surf")
+            .build()
+
+
     @JvmStatic
     fun main(args: Array<String>) {
-        val path = args[0]
-        Files.newBufferedReader(Paths.get(path)).use { reader ->
-            val config = JSON.parse(Config.serializer(), reader.readText())
-            loadAndPersist(config)
+        loadAndRead()
+    }
+
+    fun loadAndRead() {
+
+        val vector = CottontailGrpc.Vector.newBuilder().setDoubleVector(CottontailGrpc.DoubleVector.newBuilder().addAllVector(VectorUtility.randomDoubleVector(512).asIterable()))
+        val queryKnn = CottontailGrpc.Query.newBuilder()
+                .setFrom(CottontailGrpc.From.newBuilder().setEntity(entity).build())
+                .setKnn(CottontailGrpc.Knn.newBuilder().setAttribute("feature").setDistance(CottontailGrpc.Knn.Distance.L2).setK(10000).setQuery(vector))
+                .setProjection(CottontailGrpc.Projection.newBuilder().setOp(CottontailGrpc.Projection.Operation.SELECT).addAttributes("id").build())
+
+
+        val inPredicate = CottontailGrpc.AtomicLiteralBooleanPredicate.newBuilder()
+                .setAttribute("id")
+                .setOp(CottontailGrpc.AtomicLiteralBooleanPredicate.Operator.IN)
+                .addData(DataHelper.toData("v_03948_2"))
+                .addData(DataHelper.toData("v_03948_4"))
+                .addData(DataHelper.toData("test"))
+
+        val queryWhere = CottontailGrpc.Query.newBuilder()
+                .setFrom(CottontailGrpc.From.newBuilder().setEntity(entity).build())
+                .setWhere(CottontailGrpc.Where.newBuilder().setAtomic(inPredicate))
+                .setProjection(CottontailGrpc.Projection.newBuilder().setOp(CottontailGrpc.Projection.Operation.SELECT).addAttributes("id").addAttributes("feature").build())
+
+
+
+
+        val results = this.dqlService.query(CottontailGrpc.QueryMessage.newBuilder().setQuery(queryWhere).build()).forEach {
+            it.queryId
         }
     }
 
-    fun loadAndRead(config: Config) {
+    /**
+     *
+     */
+    fun loadAndPersist() {
 
-        val catalogue = Catalogue(config)
-        val schema = catalogue.getSchema("cottontail")
-        val entity = schema.getEntity("test")
+        val idColumn = CottontailGrpc.ColumnDefinition.newBuilder().setName("id").setType(CottontailGrpc.Type.STRING).setLength(-1).build()
+        val featureColumn = CottontailGrpc.ColumnDefinition.newBuilder().setName("feature").setType(CottontailGrpc.Type.FLOAT_VEC).setLength(512).build()
 
-        val engine = ch.unibas.dmi.dbis.cottontail.execution.ExecutionEngine(config)
+        /* Create schema and entity. */
+        ddlService.createSchema(schema)
+        ddlService.createEntity(CottontailGrpc.CreateEntityMessage.newBuilder().setEntity(entity).addColumns(idColumn).addColumns(featureColumn).build())
 
-        val plan = engine.newExecutionPlan()
-        val d = 512
-        val n = 10000
-
-
-        //val kNNTask = FullscanFloatKnnTask(entity, ColumnDef.withAttributes("feature","FLOAT_VEC", d), VectorUtility.randomFloatVector(d), Distance.L2, n)
-
-        //plan.addTask(kNNTask)
-        //plan.addTask(ColumnProjectionTask(entity, ColumnDef.withAttributes("id","STRING")), kNNTask.id)
-
-
-        val results = plan.execute()
-        println("Success!")
-    }
-
-
-    fun loadAndPersist(config: Config) {
-        val catalogue = Catalogue(config)
-        catalogue.createSchema("cottontail")
-
-        /* Load schema. */
-        val schema = catalogue.getSchema("cottontail")
-        schema.createEntity("test",  ColumnDef.withAttributes("id","STRING"),  ColumnDef.withAttributes("feature","FLOAT_VEC", 2048))
-        val entity = schema.getEntity("test")
 
         for (i in 1..76) {
-            val path = Paths.get("/Users/gassra02/Downloads/data/features_conceptmasksade20k_$i.json")
+            val path = Paths.get("/Volumes/Data (Mac)/Extracted/features_surfmf25k512_$i.json")
             if (Files.exists(path)) {
                 Files.newBufferedReader(path).use { reader ->
                     val gson = GsonBuilder().create()
                     val features = gson.fromJson(reader, Array<Feature>::class.java)
                     val start = System.currentTimeMillis()
                     try {
-                        entity.Tx(readonly = false).begin {
-                            for (f in features) {
-                                it.insert(StandaloneRecord(null, arrayOf(ColumnDef.withAttributes("id","STRING"), ColumnDef.withAttributes("feature","FLOAT_VEC", 2048)), arrayOf(f.id, f.feature)))
+                        val batchSize = 300
+                        for (a in 0 until features.size/batchSize) {
+                            val insert = CottontailGrpc.InsertMessage.newBuilder().setEntity(entity)
+                            for (b in a*batchSize until Math.min(features.size, a*batchSize+batchSize)) {
+                                val feature = features[b]
+                                insert.addTuple(CottontailGrpc.Tuple.newBuilder().putData("id", DataHelper.toData(feature.id)).putData("feature", DataHelper.toData(feature.feature)))
                             }
-                            true
+                            dmlService.insert(insert.build())
                         }
+
                         println("Writing ${features.size} vectors took ${System.currentTimeMillis() - start} ms.")
                     } catch(e: DatabaseException) {
                         println("Error persisting data fil file $path: ${e.message}.")
