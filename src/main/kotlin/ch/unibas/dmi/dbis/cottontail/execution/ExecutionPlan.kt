0c07256971a699396ccd8ac0df916e6886a8288a
Ralph Gasser <rg@pontius.ch>
2019-02-17 17:47:20 +0100

    Fixed log message.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/execution/ExecutionPlan.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/execution/ExecutionPlan.kt
@@ -110,7 +110,7 @@ class ExecutionPlan(executor: ExecutorService) {
 
             /* Log successful execution. */
             val output = this.first() ?: throw ExecutionPlanException(this@ExecutionPlan, "Final stage in execution plan failed!")
-            LOGGER.debug("Execution plan ${this@ExecutionPlan.id} successful! Produced ${output.rowCount} x ${output.columnCount} record set.")
+            LOGGER.debug("Execution plan ${this@ExecutionPlan.id} was run successful! It produced a ${output.rowCount} x ${output.columnCount} record set.")
             return output
         }
     }
