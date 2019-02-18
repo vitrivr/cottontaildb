47ff89004cb1f5a3c60d8eca1e5ac99fc3072f29
Ralph Gasser <rg@pontius.ch>
2019-02-18 06:21:17 +0100

    Added weighted versions of distance functions to DistanceFunction interface definition.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/knn/metrics/DistanceFunction.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/knn/metrics/DistanceFunction.kt
@@ -1,23 +1,90 @@
 package ch.unibas.dmi.dbis.cottontail.knn.metrics
 
+
+/**
+ * Interface implemented by [DistanceFunction]s that can be used to calculate the distance between two vectors.
+ *
+ * @author Ralph Gasser
+ * @version 1.0
+ */
 interface DistanceFunction {
     /**
+     * Calculates the weighted distance between two [FloatArray]s
      *
+     * @param a First [FloatArray]
+     * @param b Second [FloatArray]
+     * @param weights The [FloatArray] containing the weights.
+     *
+     * @return Distance between a and b.
+     */
+    fun invoke(a: FloatArray, b: FloatArray, weights: FloatArray): Double
+
+    /**
+     * Calculates the weighted distance between two [DoubleArray]s
+     *
+     * @param a First [DoubleArray]
+     * @param b Second [DoubleArray]
+     * @param weights The [DoubleArray] containing the weights.
+     *
+     * @return Distance between a and b.
+     */
+    fun invoke(a: DoubleArray, b: DoubleArray, weights: DoubleArray)
+
+    /**
+     * Calculates the weighted distance between two [LongArray]s
+     *
+     * @param a First [LongArray]
+     * @param b Second [LongArray]
+     * @param weights The [LongArray] containing the weights.
+     *
+     * @return Distance between a and b.
+     */
+    fun invoke(a: LongArray, b: LongArray, weights: LongArray)
+
+    /**
+     * Calculates the weighted distance between two [IntArray]s
+     *
+     * @param a First [IntArray]
+     * @param b Second [IntArray]
+     * @param weights The [IntArray] containing the weights.
+     *
+     * @return Distance between a and b.
+     */
+    fun invoke(a: IntArray, b: IntArray, weights: IntArray)
+
+    /**
+     * Calculates the distance between two [FloatArray]s
+     *
+     * @param a First [FloatArray]
+     * @param b Second [FloatArray]
+     * @return Distance between a and b.
      */
     operator fun invoke(a: FloatArray, b: FloatArray): Double
 
     /**
+     * Calculates the distance between two [DoubleArray]s
      *
+     * @param a First [DoubleArray]
+     * @param b Second [DoubleArray]
+     * @return Distance between a and b.
      */
     operator fun invoke(a: DoubleArray, b: DoubleArray): Double
 
     /**
+     * Calculates the distance between two [LongArray]s
      *
+     * @param a First [LongArray]
+     * @param b Second [LongArray]
+     * @return Distance between a and b.
      */
     operator fun invoke(a: LongArray, b: LongArray): Double
 
     /**
+     * Calculates the distance between two [IntArray]s
      *
+     * @param a First [IntArray]
+     * @param b Second [IntArray]
+     * @return Distance between a and b.
      */
     operator fun invoke(a: IntArray, b: IntArray): Double
 }
