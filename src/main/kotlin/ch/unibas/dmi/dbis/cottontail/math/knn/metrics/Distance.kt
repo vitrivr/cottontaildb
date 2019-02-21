e2d71b512d657d3693a6992f9e0ea06cd2bb504e
Luca Rossetto <luca.rossetto@unibas.ch>
2019-02-21 16:42:47 +0100

    Added Chi-Squared distance

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/math/knn/metrics/Distance.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/math/knn/metrics/Distance.kt
@@ -200,5 +200,100 @@ enum class Distance : DistanceFunction {
             }
             return b.sum().toDouble()
         }
+    },
+
+
+    /**
+     * Chi Squared distance between two vectors. Vectors must be of the same size!
+     */
+    CHISQUARED {
+        override fun invoke(a: FloatArray, b: FloatArray, weights: FloatArray): Double {
+            for (i in 0 until b.size) {
+                if (Math.abs(a[i] + b[i]) < 1e-6) {
+                    b[i] = 0f
+                }else{
+                    b[i] = ((b[i] - a[i]) * (b[i] - a[i])) / (b[i] + a[i]) * weights[i]
+                }
+
+            }
+            return b.sum().toDouble()
+        }
+
+        override fun invoke(a: DoubleArray, b: DoubleArray, weights: DoubleArray): Double {
+            for (i in 0 until b.size) {
+                if (Math.abs(a[i] + b[i]) < 1e-6) {
+                    b[i] = 0.0
+                } else {
+                    b[i] = ((b[i] - a[i]) * (b[i] - a[i])) / (b[i] + a[i]) * weights[i]
+                }
+            }
+            return b.sum()
+        }
+
+        override fun invoke(a: LongArray, b: LongArray, weights: FloatArray): Double {
+            var sum = 0.0
+            for (i in 0 until b.size) {
+                if((a[i] + b[i]) == 0L){
+                    continue
+                }
+                sum += ((b[i] - a[i]) * (b[i] - a[i])) / (b[i] + a[i]) * weights[i]
+            }
+            return sum
+        }
+
+        override fun invoke(a: IntArray, b: IntArray, weights: FloatArray): Double {
+            var sum = 0.0
+            for (i in 0 until b.size) {
+                if((a[i] + b[i]) == 0){
+                    continue
+                }
+                sum += ((b[i] - a[i]) * (b[i] - a[i])) / (b[i] + a[i]) * weights[i]
+            }
+            return sum
+        }
+
+        override fun invoke(a: FloatArray, b: FloatArray): Double {
+            for (i in 0 until b.size) {
+                if (Math.abs(a[i] + b[i]) < 1e-6) {
+                    b[i] = 0f
+                } else {
+                    b[i] = ((b[i] - a[i]) * (b[i] - a[i])) / (b[i] + a[i])
+                }
+            }
+            return b.sum().toDouble()
+        }
+
+        override fun invoke(a: DoubleArray, b: DoubleArray): Double {
+            for (i in 0 until b.size) {
+                if (Math.abs(a[i] + b[i]) < 1e-6) {
+                    b[i] = 0.0
+                } else {
+                    b[i] = ((b[i] - a[i]) * (b[i] - a[i])) / (b[i] + a[i])
+                }
+            }
+            return b.sum()
+        }
+
+        override fun invoke(a: LongArray, b: LongArray): Double {
+            var sum = 0.0
+            for (i in 0 until b.size) {
+                if((a[i] + b[i]) == 0L){
+                    continue
+                }
+                sum += ((b[i] - a[i]) * (b[i] - a[i])) / (b[i] + a[i])
+            }
+            return sum
+        }
+
+        override fun invoke(a: IntArray, b: IntArray): Double {
+            var sum = 0.0
+            for (i in 0 until b.size) {
+                if((a[i] + b[i]) == 0){
+                    continue
+                }
+                sum += ((b[i] - a[i]) * (b[i] - a[i])) / (b[i] + a[i])
+            }
+            return sum
+        }
     }
 }
