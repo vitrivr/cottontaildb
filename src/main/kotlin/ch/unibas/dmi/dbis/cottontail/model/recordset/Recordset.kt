82992d7af80d6132685fc91ec533f4de4a542955
Ralph Gasser <rg@pontius.ch>
2019-04-23 07:21:49 +0200

    Added methods to build UNION and INTERSECTION between two Recordsets.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/model/recordset/Recordset.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/model/recordset/Recordset.kt
@@ -8,6 +8,7 @@ import ch.unibas.dmi.dbis.cottontail.model.basics.Record
 import ch.unibas.dmi.dbis.cottontail.model.basics.Scanable
 import ch.unibas.dmi.dbis.cottontail.model.exceptions.QueryException
 import ch.unibas.dmi.dbis.cottontail.model.values.Value
+import java.lang.IllegalArgumentException
 
 import java.util.*
 import java.util.concurrent.atomic.AtomicLong
@@ -26,12 +27,11 @@ import java.util.concurrent.atomic.AtomicLong
  * @version 1.0
  */
 internal class Recordset(val columns: Array<ColumnDef<*>>) : Scanable, Filterable {
-
-    /** List of all the [Record]s contained in this [Recordset]. */
-    private val list: LinkedList<Record> = LinkedList()
-
     /** Internal counter for maximum tupleId. */
     private val maxTupleId = AtomicLong(0)
+
+    /** Map of all the [Record]s contained in this [Recordset]. */
+    private val map = TreeMap<Long,Record>()
 
     /** The number of columns contained in this [Recordset]. */
     val columnCount: Int
@@ -39,15 +39,17 @@ internal class Recordset(val columns: Array<ColumnDef<*>>) : Scanable, Filterabl
 
     /** The number of rows contained in this [Recordset]. */
     val rowCount: Int
-        get() = this.list.size
+        get() = this.map.size
 
     /**
      * Creates and appends a new [Record] (without a tupleId) given the provided values and appends it to this [Recordset].
      *
      * @param values The values to add to this [Recordset].
      */
+    @Synchronized
     fun addRow(values: Array<Value<*>?>) {
-        this.list.add(DatasetRecord(this.maxTupleId.incrementAndGet()).assign(values))
+        val tupleId = this.maxTupleId.incrementAndGet()
+        this.map[tupleId] = RecordsetRecord(tupleId).assign(values)
     }
 
     /**
@@ -56,8 +58,9 @@ internal class Recordset(val columns: Array<ColumnDef<*>>) : Scanable, Filterabl
      * @param tupleId The tupleId of the new [Record].
      * @param values The values to add to this [Recordset].
      */
+    @Synchronized
     fun addRow(tupleId: Long, values: Array<Value<*>?>) {
-        this.list.add(DatasetRecord(tupleId).assign(values))
+        this.map[tupleId] = RecordsetRecord(tupleId).assign(values)
     }
 
     /**
@@ -69,45 +72,93 @@ internal class Recordset(val columns: Array<ColumnDef<*>>) : Scanable, Filterabl
      * @param values The values to add to this [Recordset].
      * @return True if [Record] was added, false otherwise.
      */
+    @Synchronized
     fun addRowIf(tupleId: Long, predicate: BooleanPredicate, values: Array<Value<*>?>): Boolean {
-        val record = DatasetRecord(tupleId).assign(values)
+        val record = RecordsetRecord(tupleId).assign(values)
         return if (predicate.matches(record)) {
-            this.list.add(record)
+            this.map[tupleId] = record
+            true
         } else {
             false
         }
     }
 
     /**
-     * Retrieves the value for the specified [ColumnDef] from this [Record].
+     * Retrieves the value for the specified tuple ID from this [Recordset].
      *
-     * @param column The [ColumnDef] for which to retrieve the value.
-     * @return The value for the [ColumnDef]
+     * @param tupleId The tuple ID for which to return the [Record]
+     * @return The [Record]
      */
-    operator fun get(index: Int): Record = this.list[index]
+    @Synchronized
+    operator fun get(tupleId: Long): Record? = this.map[tupleId]
+
+    /**
+     * Creates and returns a new [Recordset] by building the union between this and the provided [Recordset]
+     *
+     * @param other The [Recordset] to union this [Recordset] with.
+     * @return combined [Recordset]
+     */
+    fun union(other: Recordset): Recordset {
+        if (!other.columns.contentDeepEquals(this.columns)) {
+            throw IllegalArgumentException("")
+        }
+
+        return Recordset(this.columns).also {new ->
+            other.forEach {
+                new.addRow(it.tupleId, it.values)
+            }
+        }
+    }
+
+    /**
+     * Creates and returns a new [Recordset] by building the intersection between this and the provided [Recordset]
+     *
+     * @param other The [Recordset] to intersect this [Recordset] with.
+     * @return combined [Recordset]
+     */
+    fun intersect(other: Recordset): Recordset {
+        if (!other.columns.contentDeepEquals(this.columns)) {
+            throw IllegalArgumentException("")
+        }
+
+        val records = this.map.keys.intersect(other.map.keys)
+        return Recordset(this.columns).also {new ->
+            records.forEach {
+                if (this.map.containsKey(it)) {
+                    new.addRow(it, this.map[it]!!.values)
+                } else {
+                    new.addRow(it, other.map[it]!!.values)
+                }
+
+            }
+        }
+    }
 
     /**
      * Applies the provided action to each [Record] in this [Recordset].
      *
      * @param action The action that should be applied.
      */
-    override fun forEach(action: (Record) -> Unit) = this.list.forEach(action)
+    @Synchronized
+    override fun forEach(action: (Record) -> Unit) = this.map.values.forEach(action)
 
     /**
      * Applies the provided mapping function to each [Record] in this [Recordset].
      *
      * @param action The mapping function that should be applied.
      */
-    override fun <R> map(action: (Record) -> R): Collection<R> = this.list.map(action)
+    @Synchronized
+    override fun <R> map(action: (Record) -> R): Collection<R> = this.map.values.map(action)
 
     /**
      * Applies the provided action to each [Record] that matches the given [Predicate].
      *
      * @param action The action that should be applied.
      */
+    @Synchronized
     override fun forEach(predicate: Predicate, action: (Record) -> Unit) {
         if (predicate is BooleanPredicate) {
-            this.list.asSequence().filter { predicate.matches(it) }.forEach { action(it) }
+            this.map.values.asSequence().filter { predicate.matches(it) }.forEach { action(it) }
         } else {
             throw QueryException.UnsupportedPredicateException("Only boolean predicates are supported for invocation of forEach() on a Recordset.")
         }
@@ -119,10 +170,11 @@ internal class Recordset(val columns: Array<ColumnDef<*>>) : Scanable, Filterabl
      * @param predicate [Predicate] to filter [Record]s.
      * @param action The mapping function that should be applied.
      */
+    @Synchronized
     override fun <R> map(predicate: Predicate, action: (Record) -> R): Collection<R> {
         if (predicate is BooleanPredicate) {
             val list = mutableListOf<R>()
-            this.list.asSequence().filter { predicate.matches(it) }.map(action).forEach { list.add(it) }
+            this.map.values.asSequence().filter { predicate.matches(it) }.map(action).forEach { list.add(it) }
             return list
         } else {
             throw QueryException.UnsupportedPredicateException("Only boolean predicates are supported for invocation of map() on a Recordset.")
@@ -135,10 +187,11 @@ internal class Recordset(val columns: Array<ColumnDef<*>>) : Scanable, Filterabl
      * @param predicate [Predicate] to filter [Record]s.
      * @return New [Filterable]
      */
+    @Synchronized
     override fun filter(predicate: Predicate): Recordset {
         if (predicate is BooleanPredicate) {
             val recordset = Recordset(this.columns)
-            this.list.asSequence().filter { predicate.matches(it) }.forEach { recordset.addRow(it.values) }
+            this.map.values.asSequence().filter { predicate.matches(it) }.forEach { recordset.addRow(it.values) }
             return recordset
         } else {
             throw QueryException.UnsupportedPredicateException("Only boolean predicates are supported for invocation of filter() on a Recordset.")
@@ -150,14 +203,16 @@ internal class Recordset(val columns: Array<ColumnDef<*>>) : Scanable, Filterabl
      *
      * @return The [List] underpinning this [Recordset].
      */
-    fun toList(): List<Record> = this.list
+    @Synchronized
+    fun toList(): List<Record> = this.map.values.toList()
 
     /**
      * Returns an [Iterator] of this [Recordset]
      *
      * @return [Iterator] of this [Recordset].
      */
-    fun iterator(): Iterator<Record> = this.list.iterator()
+    @Synchronized
+    fun iterator(): Iterator<Record> = this.map.values.iterator()
 
     /**
      * A [Record] implementation that depends on the existence of the enclosing [Recordset].
@@ -165,7 +220,7 @@ internal class Recordset(val columns: Array<ColumnDef<*>>) : Scanable, Filterabl
      * @author Ralph Gasser
      * @version 1.0
      */
-    inner class DatasetRecord(override val tupleId: Long, init: Array<Value<*>?>? = null) : Record {
+    inner class RecordsetRecord(override val tupleId: Long, init: Array<Value<*>?>? = null) : Record {
 
         /** Array of [ColumnDef]s that describes the [Columns] of this [Record]. */
         override val columns: Array<out ColumnDef<*>>
