c12e9864a1b7c0877589d3e03c5c9988942c26dc
Ralph Gasser <rg@pontius.ch>
2019-05-11 14:00:36 +0200

    Added possibility to create standalone executable.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/Cottontail.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/Cottontail.kt
@@ -4,31 +4,29 @@ import ch.unibas.dmi.dbis.cottontail.config.Config
 import ch.unibas.dmi.dbis.cottontail.database.catalogue.Catalogue
 import ch.unibas.dmi.dbis.cottontail.execution.ExecutionEngine
 import ch.unibas.dmi.dbis.cottontail.server.grpc.CottontailGrpcServer
+import kotlinx.serialization.UnstableDefault
 
-import kotlinx.serialization.json.JSON
+import kotlinx.serialization.json.Json.Companion.parse
 
 import java.nio.file.Files
 import java.nio.file.Paths
 
-object Cottontail {
-
-    /**
-     * Entry point for Cottontail DB demon.
-     *
-     * @param args Program arguments.
-     */
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val path = args[0]
-        Files.newBufferedReader(Paths.get(path)).use { reader ->
-            val config = JSON.parse(Config.serializer(), reader.readText())
-            val catalogue = Catalogue(config)
-            val engine = ExecutionEngine(config.executionConfig)
-            val server = CottontailGrpcServer(config.serverConfig, catalogue, engine)
-            server.start()
-            while(server.isRunning) {
-                Thread.sleep(1000)
-            }
+/**
+ * Entry point for Cottontail DB demon.
+ *
+ * @param args Program arguments.
+ */
+@UnstableDefault
+fun main(args: Array<String>) {
+    val path = args[0]
+    Files.newBufferedReader(Paths.get(path)).use { reader ->
+        val config = parse(Config.serializer(), reader.readText())
+        val catalogue = Catalogue(config)
+        val engine = ExecutionEngine(config.executionConfig)
+        val server = CottontailGrpcServer(config.serverConfig, catalogue, engine)
+        server.start()
+        while(server.isRunning) {
+            Thread.sleep(1000)
         }
     }
 }
