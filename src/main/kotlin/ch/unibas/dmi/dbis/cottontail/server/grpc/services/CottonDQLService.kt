c0d7846d3ca87372b5a94c93e72b93760962e733
Ralph Gasser <ralph.gasser@unibas.ch>
2019-06-07 19:59:02 +0200

    Added implementation for batched queries.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/server/grpc/services/CottonDQLService.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/server/grpc/services/CottonDQLService.kt
@@ -14,6 +14,9 @@ import ch.unibas.dmi.dbis.cottontail.utilities.math.BitUtil
 import com.google.protobuf.Empty
 import io.grpc.Status
 import io.grpc.stub.StreamObserver
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import java.util.*
 
@@ -69,7 +72,7 @@ internal class CottonDQLService (val catalogue: Catalogue, val engine: Execution
             }
             LOGGER.trace("Sending back ${results.rowCount} rows for query $queryId took ${System.currentTimeMillis()-startSending}ms.")
         } else {
-            LOGGER.trace("Query yielded now results.")
+            LOGGER.trace("Query yielded no results.")
         }
 
         /* Complete query. */
@@ -91,8 +94,70 @@ internal class CottonDQLService (val catalogue: Catalogue, val engine: Execution
     /**
      *  gRPC endpoint for handling batched queries.
      */
-    override fun batchedQuery(request: CottontailGrpc.BatchedQueryMessage, responseObserver: StreamObserver<CottontailGrpc.QueryResponseMessage>) {
-        super.batchedQuery(request, responseObserver)
+    override fun batchedQuery(request: CottontailGrpc.BatchedQueryMessage, responseObserver: StreamObserver<CottontailGrpc.QueryResponseMessage>) = try {
+        /* Start the query by giving the start signal. */
+        val start = System.currentTimeMillis()
+        val queryId = UUID.randomUUID().toString()
+
+        /* Run queries from batch in parallel. */
+        runBlocking {
+            request.queriesList.forEachIndexed { index, query ->
+                launch (context = Dispatchers.Default) {
+                    /* Bind query and generate execution plan */
+                    val startBinding = System.currentTimeMillis()
+                    val binder = GrpcQueryBinder(catalogue = this@CottonDQLService.catalogue, engine = this@CottonDQLService.engine)
+                    val plan = binder.parseAndBind(query)
+                    LOGGER.trace("Parsing & binding query $index of batch $queryId took ${System.currentTimeMillis()-startBinding}ms.")
+
+                    /* Start the query by giving the start signal. */
+                    responseObserver.onNext(CottontailGrpc.QueryResponseMessage.newBuilder().setStart(true).setQueryId(queryId).build())
+
+                    /* Execute query. */
+                    val startExecution = System.currentTimeMillis()
+                    val results = plan.execute()
+                    LOGGER.trace("Executing query $index of batch $queryId took ${System.currentTimeMillis()-startExecution}ms.")
+
+                    /* Calculate batch size based on an example message and the maxMessageSize. */
+                    if (results.rowCount > 0) {
+                        val startSending = System.currentTimeMillis()
+                        val first = results.first()
+                        if (first != null) {
+                            val exampleSize = BitUtil.nextPowerOfTwo(recordToTuple(first).build().serializedSize)
+                            val pageSize = (maxMessageSize/exampleSize)
+                            val maxPages = Math.floorDiv(results.rowCount,pageSize)
+
+                            /* Return results. */
+                            val iterator = results.iterator()
+                            for (i in 0..maxPages) {
+                                val responseBuilder = CottontailGrpc.QueryResponseMessage.newBuilder().setStart(false).setPageSize(pageSize).setPage(i).setMaxPage(maxPages).setTotalHits(results.rowCount)
+                                for (j in i * pageSize until Math.min(results.rowCount, i*pageSize + pageSize)) {
+                                    responseBuilder.addResults(recordToTuple(iterator.next()))
+                                }
+                                responseObserver.onNext(responseBuilder.build())
+                            }
+                        }
+                        LOGGER.trace("Sending back ${results.rowCount} rows for query $index of batch $queryId took ${System.currentTimeMillis()-startSending}ms.")
+                    } else {
+                        LOGGER.trace("Query $index of batch $queryId yielded no results.")
+                    }
+                }
+            }
+        }
+
+        /* Complete query. */
+        LOGGER.info("Batched query $queryId took ${System.currentTimeMillis()-start}ms to complete.")
+        responseObserver.onCompleted()
+    } catch (e: QueryException.QuerySyntaxException) {
+        responseObserver.onError(Status.INVALID_ARGUMENT.withDescription("Query syntax is invalid: ${e.message}").asException())
+    } catch (e: QueryException.QueryBindException) {
+        responseObserver.onError(Status.INVALID_ARGUMENT.withDescription("Query binding failed: ${e.message}").asException())
+    } catch (e: ExecutionPlanException) {
+        responseObserver.onError(Status.INTERNAL.withDescription("Query execution failed because execution engine signaled an error: ${e.message}").asException())
+    }  catch (e: DatabaseException) {
+        responseObserver.onError(Status.INTERNAL.withDescription("Query execution failed because of a database error: ${e.message}").asException())
+    } catch (e: Throwable) {
+        e.printStackTrace()
+        responseObserver.onError(Status.UNKNOWN.withDescription("Query execution failed failed because of a unknown error: ${e.message}").asException())
     }
 
     /**
