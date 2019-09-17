460f377a6ae69c7506a90c9273af19fffb0ae705
Ralph Gasser <ralph.gasser@unibas.ch>
2019-09-17 09:06:05 +0200

    Fixed a few typos and removed unnecessary imports.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/config/ServerConfig.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/config/ServerConfig.kt
@@ -1,19 +1,18 @@
 package ch.unibas.dmi.dbis.cottontail.config
 
 import ch.unibas.dmi.dbis.cottontail.utilities.serializers.PathSerializer
-import kotlinx.serialization.Optional
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import java.nio.file.Path
 
 /**
- * Config for Cottontail DB's GRPC server.
+ * Config for Cottontail DB's gRPC server.
  *
  *
  * @param port The port under which Cottontail DB demon should be listening for calls. Defaults to 1865
- * @param messageSize The the maximum size an incoming GRPC message can have. Defaults to 4'194'304 bytes (4096 kbytes).
- * @param coreThreads The core size of the thread pool used to run the GRPC server.
- * @param maxThreads The maximum number of threads that handle calls to the GRPC server.
+ * @param messageSize The the maximum size an incoming gRPC message can have. Defaults to 4'194'304 bytes (4096 kbytes).
+ * @param coreThreads The core size of the thread pool used to run the gRPC server.
+ * @param maxThreads The maximum number of threads that handle calls to the gRPC server.
  * @param keepAliveTime The number of milliseconds to wait before decommissioning unused threads.
  * @param certFile Path to the certificate file used for TLS.
  * @param privateKey Path to the private key used for TLS.
@@ -32,7 +31,7 @@ data class ServerConfig(
     @Serializable(with=PathSerializer::class) val privateKey: Path? = null) {
 
     /**
-     * True if TLS should be used for GRPC communication, false otherwise.
+     * True if TLS should be used for gRPC communication, false otherwise.
      */
     @Transient val useTls
         get() = this.certFile != null && this.privateKey != null
