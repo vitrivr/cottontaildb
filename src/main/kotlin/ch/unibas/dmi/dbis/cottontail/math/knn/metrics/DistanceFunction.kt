558292efa90c30fe3d2c42a24f0e7a7ec56e3213
Ralph Gasser <ralph.gasser@unibas.ch>
2019-09-17 09:15:20 +0200

    Fixed comments.

--- src/main/kotlin/ch/unibas/dmi/dbis/cottontail/math/knn/metrics/DistanceFunction.kt
+++ src/main/kotlin/ch/unibas/dmi/dbis/cottontail/math/knn/metrics/DistanceFunction.kt
@@ -1,14 +1,12 @@
 package ch.unibas.dmi.dbis.cottontail.math.knn.metrics
 
 import ch.unibas.dmi.dbis.cottontail.model.values.VectorValue
-import java.util.*
-
 
 /**
  * Interface implemented by [DistanceFunction]s that can be used to calculate the distance between two vectors.
  *
  * @author Ralph Gasser
- * @version 1.0
+ * @version 1.1
  */
 interface DistanceFunction<T> {
     /**
@@ -17,23 +15,21 @@ interface DistanceFunction<T> {
     val operations: Int
 
     /**
-     * Calculates the weighted distance between two [BitSet]s, i.e. [BooleanArray]'s where
-     * each element can either be 1 or 0.
+     * Calculates the weighted distance between two [VectorValue].
      *
-     * @param a First [BitSet]
-     * @param b Second [BitSet]
-     * @param weights The [Float] containing the weights.
+     * @param a First [VectorValue]
+     * @param b Second [VectorValue]
      *
      * @return Distance between a and b.
      */
     operator fun invoke(a: VectorValue<T>, b: VectorValue<T>): Double
 
     /**
-     * Calculates the weighted distance between two [FloatArray]s
+     * Calculates the weighted distance between two [VectorValue]s
      *
-     * @param a First [FloatArray]
-     * @param b Second [FloatArray]
-     * @param weights The [FloatArray] containing the weights.
+     * @param a First [VectorValue]
+     * @param b Second [VectorValue]
+     * @param weights The [VectorValue] containing the weights.
      *
      * @return Distance between a and b.
      */
