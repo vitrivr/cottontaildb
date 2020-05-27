ffaa9ce552996ce6a3932ef13d1ea991199ea7ed
Ralph Gasser <rg@pontius.ch>
2020-05-27 09:56:31 +0200

    Minor optimisation for #20 as suggested by @mpas97
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/execution/tasks/recordset/projection/RecordsetDistinctTask.kt
+++ src/main/kotlin/org/vitrivr/cottontail/execution/tasks/recordset/projection/RecordsetDistinctTask.kt
@@ -3,11 +3,8 @@ package org.vitrivr.cottontail.execution.tasks.recordset.projection
 import com.github.dexecutor.core.task.Task
 import com.github.dexecutor.core.task.TaskExecutionException
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet
-import org.eclipse.collections.impl.set.mutable.primitive.IntHashSet
-import org.eclipse.collections.impl.set.mutable.primitive.LongHashSet
 import org.vitrivr.cottontail.execution.tasks.basics.ExecutionTask
 import org.vitrivr.cottontail.model.recordset.Recordset
-import org.vitrivr.cottontail.utilities.name.Name
 
 /**
  * A [Task] that makes sure, that a [Recordset] only contains distinct values.
@@ -15,7 +12,7 @@ import org.vitrivr.cottontail.utilities.name.Name
  * @author Ralph Gasser
  * @version 1.0
  */
-class RecordsetDistinctTask() : ExecutionTask("RecordsetDistinctTask") {
+class RecordsetDistinctTask : ExecutionTask("RecordsetDistinctTask") {
     /**
      * Executes this [RecordsetDistinctTask]
      */
@@ -23,7 +20,8 @@ class RecordsetDistinctTask() : ExecutionTask("RecordsetDistinctTask") {
         assertUnaryInput()
 
         /* Get records from parent task. */
-        val parent = this.first() ?: throw TaskExecutionException("DISTINCT projection could not be executed because parent task has failed.")
+        val parent = this.first()
+                ?: throw TaskExecutionException("DISTINCT projection could not be executed because parent task has failed.")
         val result = Recordset(parent.columns, parent.rowCount)
 
         /* Generate a hash per entry and make sure that hash remains unique. */
@@ -31,8 +29,7 @@ class RecordsetDistinctTask() : ExecutionTask("RecordsetDistinctTask") {
         parent.forEach {r ->
             var hash = 0L
             r.columns.map { c -> hash = (c.hashCode() * hash) + r[c].hashCode() }
-            if (!seen.contains(hash)) {
-                seen.add(hash)
+            if (seen.add(hash)) {
                 result.addRowUnsafe(r.values)
             }
         }
