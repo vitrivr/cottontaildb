0862da558b243e917b828d9e3ba5133c95c875d0
Ralph Gasser <rg@pontius.ch>
2020-06-07 12:33:31 +0200

    DoubleVectorDistanceTests are now parametrized.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/test/kotlin/org/vitrivr/cottontail/math/knn/DoubleVectorDistanceTest.kt
+++ src/test/kotlin/org/vitrivr/cottontail/math/knn/DoubleVectorDistanceTest.kt
@@ -1,9 +1,10 @@
-package org.vitrivr.cottontail.math
+package org.vitrivr.cottontail.math.knn
 
 import org.apache.commons.math3.util.MathArrays
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import org.vitrivr.cottontail.math.knn.metrics.EuclidianDistance
 import org.vitrivr.cottontail.math.knn.metrics.ManhattanDistance
 import org.vitrivr.cottontail.math.knn.metrics.SquaredEuclidianDistance
@@ -15,21 +16,26 @@ import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
-
+/**
+ * Test cases that test for correctness of some basic distance calculations with [DoubleVectorValue].
+ *
+ * @author Ralph Gasser
+ * @version 1.0
+ */
 class DoubleVectorDistanceTest {
 
     companion object {
         const val COLLECTION_SIZE = 1_000_000
-        const val DELTA = 1e-12
+        const val DELTA = 1e-10
         val RANDOM = SplittableRandom()
     }
 
     @ExperimentalTime
-    @RepeatedTest(3)
-    fun testL1Distance() {
-        val dimensions = RANDOM.nextInt(2048)
-        val query = DoubleVectorValue.random(dimensions, RANDOM)
-        val collection = VectorUtility.randomDoubleVectorSequence(dimensions, COLLECTION_SIZE, RANDOM)
+    @ParameterizedTest
+    @ValueSource(ints = [32, 64, 128, 256, 512, 1024])
+    fun testL1Distance(dimension: Int) {
+        val query = DoubleVectorValue.random(dimension, RANDOM)
+        val collection = VectorUtility.randomDoubleVectorSequence(dimension, COLLECTION_SIZE, RANDOM)
 
         var sum1 = 0.0
         var sum2 = 0.0
@@ -48,7 +54,7 @@ class DoubleVectorDistanceTest {
             sum3 += MathArrays.distance1(it.data, query.data)
         }
 
-        println("Calculating L1 distance for collection (s=$COLLECTION_SIZE, d=$dimensions) took ${time1/COLLECTION_SIZE} (optimized) resp. ${time2/COLLECTION_SIZE}  per vector on average.")
+        println("Calculating L1 distance for collection (s=$COLLECTION_SIZE, d=$dimension) took ${time1 / COLLECTION_SIZE} (optimized) resp. ${time2 / COLLECTION_SIZE}  per vector on average.")
 
         assertTrue(time1 < time2, "Optimized version of L1 is slower than default version!")
         assertTrue(sum1 / sum3 < 1.0 + DELTA, "Deviation for optimized version detected. Expected: $sum3, Received: $sum1")
@@ -58,11 +64,11 @@ class DoubleVectorDistanceTest {
     }
 
     @ExperimentalTime
-    @RepeatedTest(3)
-    fun testL2SquaredDistance() {
-        val dimensions = RANDOM.nextInt(2048)
-        val query = DoubleVectorValue.random(dimensions, RANDOM)
-        val collection = VectorUtility.randomDoubleVectorSequence(dimensions, COLLECTION_SIZE, RANDOM)
+    @ParameterizedTest
+    @ValueSource(ints = [32, 64, 128, 256, 512, 1024])
+    fun testL2SquaredDistance(dimension: Int) {
+        val query = DoubleVectorValue.random(dimension, RANDOM)
+        val collection = VectorUtility.randomDoubleVectorSequence(dimension, COLLECTION_SIZE, RANDOM)
 
         var sum1 = 0.0
         var sum2 = 0.0
@@ -81,7 +87,7 @@ class DoubleVectorDistanceTest {
             sum3 += MathArrays.distance(it.data, query.data).pow(2)
         }
 
-        println("Calculating L2^2 distance for collection (s=$COLLECTION_SIZE, d=$dimensions) took ${time1/COLLECTION_SIZE} (optimized) resp. ${time2/COLLECTION_SIZE}  per vector on average.")
+        println("Calculating L2^2 distance for collection (s=$COLLECTION_SIZE, d=$dimension) took ${time1 / COLLECTION_SIZE} (optimized) resp. ${time2 / COLLECTION_SIZE}  per vector on average.")
 
         assertTrue(time1 < time2, "Optimized version of L2^2 is slower than default version!")
         assertEquals(sum3 , sum1, "L2^2 for optimized version does no equal expected value.")
@@ -93,11 +99,11 @@ class DoubleVectorDistanceTest {
     }
 
     @ExperimentalTime
-    @RepeatedTest(3)
-    fun testL2Distance() {
-        val dimensions = RANDOM.nextInt(2048)
-        val query = DoubleVectorValue.random(dimensions, RANDOM)
-        val collection = VectorUtility.randomDoubleVectorSequence(dimensions, COLLECTION_SIZE, RANDOM)
+    @ParameterizedTest
+    @ValueSource(ints = [32, 64, 128, 256, 512, 1024])
+    fun testL2Distance(dimension: Int) {
+        val query = DoubleVectorValue.random(dimension, RANDOM)
+        val collection = VectorUtility.randomDoubleVectorSequence(dimension, COLLECTION_SIZE, RANDOM)
 
         var sum1 = 0.0
         var sum2 = 0.0
@@ -116,7 +122,7 @@ class DoubleVectorDistanceTest {
             sum3 += MathArrays.distance(it.data, query.data)
         }
 
-        println("Calculating L2 distance for collection (s=$COLLECTION_SIZE, d=$dimensions) took ${time1/COLLECTION_SIZE} (optimized) resp. ${time2/COLLECTION_SIZE} per vector on average.")
+        println("Calculating L2 distance for collection (s=$COLLECTION_SIZE, d=$dimension) took ${time1 / COLLECTION_SIZE} (optimized) resp. ${time2 / COLLECTION_SIZE} per vector on average.")
 
         assertTrue(time1 < time2, "Optimized version of L2 is slower than default version!")
         assertEquals(sum3 , sum1, "L2 for optimized version does not equal expected value.")
