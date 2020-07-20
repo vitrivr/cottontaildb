0c46808f102a790724d22d8109906c243c55ad53
Loris Sauter <loris.sauter@unibas.ch>
2020-07-20 13:55:40 +0200

    Adjustments in build config for executable jar

--- src/test/kotlin/org/vitrivr/cottontail/TestConstants.kt
+++ src/test/kotlin/org/vitrivr/cottontail/TestConstants.kt
@@ -5,5 +5,5 @@ import java.nio.file.Paths
 
 
 object TestConstants {
-    val config = Config(root = Paths.get("./cotton-test"))
+    val config = Config(root = Paths.get("./cotton-test"), cli=false)
 }
