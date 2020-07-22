f0d94a87699288d52ce667b9b5dbd034639616b0
Loris Sauter <loris.sauter@unibas.ch>
2020-07-22 11:08:15 +0200

    Slightly improved code maintenance and eased insertion of new commands for CLI

--- src/main/kotlin/org/vitrivr/cottontail/cli/CottontailCommand.kt
+++ src/main/kotlin/org/vitrivr/cottontail/cli/CottontailCommand.kt
@@ -89,12 +89,26 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
     /**
      * Base class for none entity specific commands (for potential future generalisation)
      */
-    abstract inner class AbstractCottontailCommand(name: String, help: String) : CliktCommand(name = name, help = help)
+    abstract inner class AbstractCottontailCommand(name: String, help: String) : CliktCommand(name = name, help = help) {
+        /**
+         * The actual command execution. Override this for your command
+         */
+        abstract fun exec()
+
+        /**
+         * Runs the command by calling [AbstractCottontailCommand.exec] safely. Exceptions are caught and printed.
+         */
+        override fun run() = try {
+            exec()
+        } catch (e: StatusRuntimeException) {
+            println("Command execution failed: ${e.message}")
+        }
+    }
 
     /**
      * Base class for entity specific commands, providing / querying from user schema and entity as arguments
      */
-    abstract inner class AbstractEntityCommand(name: String, help: String) : CliktCommand(name = name, help = help) {
+    abstract inner class AbstractEntityCommand(name: String, help: String) : AbstractCottontailCommand(name = name, help = help) {
         protected val schema: String by argument(name = "schema", help = "The schema name this command targets at")
         protected val entity: String by argument(name = "entity", help = "The entity name this command targets at")
     }
@@ -103,7 +117,7 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
      * Command to list available entities and schemata
      */
     inner class ListAllEntitiesCommand : AbstractCottontailCommand(name = "list", help = "Lists all entities in all schemata") {
-        override fun run() = try {
+        override fun exec() {
             this@CottontailCommand.ddlService.listSchemas(CottontailGrpc.Empty.getDefaultInstance()).forEach { _schema ->
                 println("Entities for Schema ${_schema.name}:")
                 this@CottontailCommand.ddlService.listEntities(_schema).forEach { _entity ->
@@ -113,8 +127,6 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
                     println("  ${_entity.name}")
                 }
             }
-        } catch (e: StatusRuntimeException) {
-            println("Command execution failed: ${e.message}")
         }
     }
 
@@ -123,7 +135,7 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
      */
     inner class PreviewEntityCommand : AbstractEntityCommand(name = "preview", help = "Gives a preview of the entity specified") {
         private val limit: Long by option("-l", "--limit", help = "Limits the amount of printed results").long().default(10).validate { require(it > 0) }
-        override fun run() = try {
+        override fun exec() {
             println("Showing first $limit elements of entity $schema.$entity")
             val qm = CottontailGrpc.QueryMessage.newBuilder().setQuery(
                     CottontailGrpc.Query.newBuilder()
@@ -136,8 +148,6 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
             query.forEach { page ->
                 println(page.resultsList.dropLast(Math.max(0, page.resultsCount - limit).toInt()))
             }
-        } catch (e: StatusRuntimeException) {
-            println("Command execution failed: ${e.message}")
         }
 
     }
@@ -146,12 +156,10 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
      * COmmand for entity details
      */
     inner class ShowEntityCommand : AbstractEntityCommand(name = "show", help = "Gives an overview of the entity and its columns") {
-        override fun run() = try {
+        override fun exec() {
             val details = this@CottontailCommand.ddlService.entityDetails(Entity(entity, Schema(schema)))
             println("Entity ${details.entity.schema.name}.${details.entity.name} with ${details.columnsCount} columns: ")
             print("  ${details.columnsList.map { "${it.name} (${it.type})" }.joinToString(", ")}")
-        } catch (e: StatusRuntimeException) {
-            println("Command execution failed: ${e.message}")
         }
     }
 
@@ -159,17 +167,15 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
      * Command to optimize an entity
      */
     inner class OptimizeEntityCommand : AbstractEntityCommand(name = "optimize", help = "Optimizes the specified entity, e.g. rebuilds the indices") {
-        override fun run() = try {
+        override fun exec() {
             println("Optimizing entity $schema.$entity")
             this@CottontailCommand.ddlService.optimizeEntity(Entity(entity, Schema(schema)))
             println("Optimization complete!")
-        } catch (e: StatusRuntimeException) {
-            println("Command execution failed: ${e.message}")
         }
     }
 
     inner class CountEntityCommand : AbstractEntityCommand(name = "count", help = "Counts the given entity's rows") {
-        override fun run() = try {
+        override fun exec() {
             println("Counting elements of entity $schema.$entity")
             val qm = CottontailGrpc.QueryMessage.newBuilder().setQuery(
                     CottontailGrpc.Query.newBuilder()
@@ -183,15 +189,13 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
                     println(page.resultsList.first())
                 }
             }
-        } catch (e: StatusRuntimeException) {
-            println("Command execution failed: ${e.message}")
         }
     }
 
     inner class QueryByColumnValueEqualsEntityCommand : AbstractEntityCommand(name = "find", help = "Find within an entity by column-value specification") {
         val col: String by option("-c", "--column", help = "Column name").required()
         val value: String by option("-v", "--value", help = "The value").required()
-        override fun run() = try {
+        override fun exec() {
             val qm = CottontailGrpc.QueryMessage.newBuilder().setQuery(
                     CottontailGrpc.Query.newBuilder()
                             .setFrom(From(Entity(entity, Schema(schema))))
@@ -211,8 +215,6 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
                 size += page.resultsCount
             }
             println("Printed $size individual results")
-        } catch (e: StatusRuntimeException) {
-            println("Command execution failed: ${e.message}")
         }
     }
 
@@ -220,11 +222,9 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
      * Command to drop, i.e. remove the given entity
      */
     inner class DropEntityCommand : AbstractEntityCommand(name = "drop", help = "Drops the given entity from the database and deletes it therefore") {
-        override fun run() = try {
+        override fun exec() {
             this@CottontailCommand.ddlService.dropEntity(Entity(entity, Schema(schema)))
             println("Dropped entity $schema.$entity")
-        } catch (e: StatusRuntimeException) {
-            println("Command execution failed: ${e.message}")
         }
     }
 
@@ -252,7 +252,7 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
      * Stops the entire application
      */
     inner class StopCommand : AbstractCottontailCommand(name = "stop", help = "Stops the database server and this CLI") {
-        override fun run() {
+        override fun exec() {
             println("Stopping now...")
             this@CottontailCommand.channel.shutdown()
             Cli.stopServer()
@@ -266,7 +266,7 @@ class CottontailCommand(private val host: String, private val port: Int) : NoOpC
     inner class ReloadCommand : AbstractCottontailCommand(name = "reload", help = "Reload the connection to the DB server") {
         val host: String by option("-s", "--server", help = "The server address.").defaultLazy { this@CottontailCommand.host }
         val port: Int by option("-p", "--port", help = "The db port").int().defaultLazy { this@CottontailCommand.port }
-        override fun run() {
+        override fun exec() {
             println("Reconnecting...")
             this@CottontailCommand.initDbCon(host, port)
             println("Reconnected.")
