b72eaa17d1e36f747206249bbfac717166ebf12c
Ralph Gasser <rg@pontius.ch>
2020-07-22 15:48:10 +0200

    Moved some utility function to Extensions.kt
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/server/grpc/helper/Extensions.kt
+++ src/main/kotlin/org/vitrivr/cottontail/server/grpc/helper/Extensions.kt
@@ -9,23 +9,44 @@ import org.vitrivr.cottontail.model.basics.Name
 /**
  * Extension function that generates the FQN for the given [CottontailGrpc.Entity].
  *
- * @return Fully qualified name for the given [CottontailGrpc.Entity]
+ * @return [Name.SchemaName] for the given [CottontailGrpc.Schema]
  */
 fun CottontailGrpc.Schema.fqn(): Name.SchemaName = Name.SchemaName(this.name)
 
 /**
  * Extension function that generates the FQN for the given [CottontailGrpc.Entity].
  *
- * @return Fully qualified name for the given [CottontailGrpc.Entity]
+ * @return [Name.EntityName] for the given [CottontailGrpc.Entity]
  */
 fun CottontailGrpc.Entity.fqn(): Name.EntityName = Name.EntityName(this.schema.name, this.name)
 
 /**
  * Extension function that generates the FQN for the given [CottontailGrpc.Entity].
  *
- * @return Fully qualified name for the given [CottontailGrpc.Entity]
+ * @return [Name.IndexName] for the given [CottontailGrpc.Index]
  */
 fun CottontailGrpc.Index.fqn(): Name.IndexName = Name.IndexName(this.entity.schema.name, this.entity.name, this.name)
+
+/**
+ * Extension function that generates the [CottontailGrpc.Entity] for the given [Name.EntityName].
+ *
+ * @return [CottontailGrpc.Entity] for the given [Name.EntityName].
+ */
+fun Name.EntityName.proto(): CottontailGrpc.Entity = CottontailGrpc.Entity.newBuilder().setName(this.simple).setSchema(this.schema().proto()).build()
+
+/**
+ * Extension function that generates the [CottontailGrpc.Schema] for the given [Name.SchemaName].
+ *
+ * @return [CottontailGrpc.Schema] for the given [Name.SchemaName].
+ */
+fun Name.SchemaName.proto() = CottontailGrpc.Schema.newBuilder().setName(this.simple).build()
+
+/**
+ * Extension function that generates the [CottontailGrpc.From] for the given [Name.EntityName].
+ *
+ * @return [CottontailGrpc.Schema] for the given [Name.SchemaName].
+ */
+fun Name.EntityName.protoFrom(): CottontailGrpc.From = CottontailGrpc.From.newBuilder().setEntity(this.proto()).build()
 
 /**
  * Extension function that generates the [KnnPredicateHint] for the given [CottontailGrpc.KnnHint].
