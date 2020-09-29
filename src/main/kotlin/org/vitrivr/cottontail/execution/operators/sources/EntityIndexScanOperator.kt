5f6d59de2fab5685a5ff7e5a1d114794c51e1ad4
Ralph Gasser <rg@pontius.ch>
2020-09-29 10:41:38 +0200

    EntityIndexScanOperator now returns all requested columns + columns returned by the index.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/execution/operators/sources/EntityIndexScanOperator.kt
+++ src/main/kotlin/org/vitrivr/cottontail/execution/operators/sources/EntityIndexScanOperator.kt
@@ -8,9 +8,11 @@ import org.vitrivr.cottontail.database.entity.Entity
 import org.vitrivr.cottontail.database.index.IndexType
 import org.vitrivr.cottontail.database.queries.components.BooleanPredicate
 import org.vitrivr.cottontail.execution.ExecutionEngine
+import org.vitrivr.cottontail.execution.exceptions.ExecutionException
 import org.vitrivr.cottontail.execution.operators.basics.OperatorStatus
 import org.vitrivr.cottontail.model.basics.ColumnDef
 import org.vitrivr.cottontail.model.basics.Record
+import org.vitrivr.cottontail.model.recordset.StandaloneRecord
 
 /**
  * An [AbstractEntityOperator] that scans an [Entity.Index] and streams all [Record]s found within.
@@ -20,14 +22,30 @@ import org.vitrivr.cottontail.model.basics.Record
  */
 class EntityIndexScanOperator(context: ExecutionEngine.ExecutionContext, entity: Entity, columns: Array<ColumnDef<*>>, private val predicate: BooleanPredicate, val indexHint: IndexType) : AbstractEntityOperator(context, entity, columns) {
 
+    /** Get [Index] that */
+    private val index = this.entity.allIndexes().filter { it.type == this.indexHint && it.canProcess(this.predicate) }.first()
+
+    /** Determine [ColumnDef] produced by this [EntityIndexScanOperator]. */
+    override val columns: Array<ColumnDef<*>> = this.index.produces + super.columns
+
+    /** Determine [ColumnDef]s that are not produced by the [Index] and require separate fetching. */
+    private val columnsToFetch = super.columns.filter { !this.index.produces.contains(it) }.toTypedArray()
+
     override fun toFlow(scope: CoroutineScope): Flow<Record> {
         check(this.status == OperatorStatus.OPEN) { "Cannot convert operator $this to flow because it is in state ${this.status}." }
         val tx = this.context.getTx(this.entity)
-        val indexTx = tx.indexes(this.predicate.columns.toTypedArray(), this.indexHint).first()
+        val indexTx = tx.index(this.index.name)
+                ?: throw ExecutionException("Could not find desired index ${this.index.name}.")
+
         return flow {
             indexTx.filter(this@EntityIndexScanOperator.predicate).use { iterator ->
                 for (record in iterator) {
-                    emit(tx.read(record.tupleId, this@EntityIndexScanOperator.columns))
+                    if (columnsToFetch.isEmpty()) {
+                        emit(record)
+                    } else {
+                        val fetched = tx.read(record.tupleId, columnsToFetch)
+                        emit(StandaloneRecord(record.tupleId, this@EntityIndexScanOperator.columns, (record.values + fetched.values)))
+                    }
                 }
             }
         }
