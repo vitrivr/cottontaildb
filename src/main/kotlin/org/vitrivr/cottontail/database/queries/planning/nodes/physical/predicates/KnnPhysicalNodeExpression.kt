fcedd14a06ca7b8333701aa3f78b2b53be9c3faa
Ralph Gasser <rg@pontius.ch>
2020-10-29 16:32:56 +0100

    Simplified KnnPhysicalNodeExpression using new partition() functionality.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/nodes/physical/predicates/KnnPhysicalNodeExpression.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/nodes/physical/predicates/KnnPhysicalNodeExpression.kt
@@ -2,8 +2,6 @@ package org.vitrivr.cottontail.database.queries.planning.nodes.physical.predicat
 
 import org.vitrivr.cottontail.database.queries.components.KnnPredicate
 import org.vitrivr.cottontail.database.queries.planning.cost.Cost
-import org.vitrivr.cottontail.database.queries.planning.nodes.interfaces.NodeExpression
-import org.vitrivr.cottontail.database.queries.planning.nodes.physical.NullaryPhysicalNodeExpression
 import org.vitrivr.cottontail.database.queries.planning.nodes.physical.UnaryPhysicalNodeExpression
 import org.vitrivr.cottontail.execution.ExecutionEngine
 import org.vitrivr.cottontail.execution.operators.basics.Operator
@@ -28,19 +26,9 @@ class KnnPhysicalNodeExpression(val knn: KnnPredicate<*>) : UnaryPhysicalNodeExp
     override fun copy() = KnnPhysicalNodeExpression(this.knn)
     override fun toOperator(context: ExecutionEngine.ExecutionContext): Operator {
         if (this.cost.cpu > 1.0f) {
-            val base = seekBase(this.input)
-            return if (base is NullaryPhysicalNodeExpression && base.canBePartitioned) {
-                val partitions = base.partition(min(this.cost.cpu.roundToInt(), context.availableThreads))
+            return if (this.input.canBePartitioned) {
+                val partitions = this.input.partition(min(this.cost.cpu.roundToInt(), context.availableThreads))
                 val operators = partitions.map {
-                    var prev: NodeExpression? = null
-                    var next: NodeExpression = this.input
-                    while (next != base) {
-                        if (prev != null) {
-                            next.addInput(prev)
-                        }
-                        prev = next.copy()
-                        next = prev.inputs.first()
-                    }
                     it.toOperator(context)
                 }
                 ParallelKnnOperator(operators, context, this.knn)
