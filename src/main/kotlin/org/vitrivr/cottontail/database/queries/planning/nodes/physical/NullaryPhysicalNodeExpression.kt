845ff140bfbe7864a59b8dfaf7ba120c465c3c8a
Ralph Gasser <rg@pontius.ch>
2020-10-29 16:32:36 +0100

    All PhysicalNodeExpressions now support canBePartitioned and the partition() method.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/nodes/physical/NullaryPhysicalNodeExpression.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/nodes/physical/NullaryPhysicalNodeExpression.kt
@@ -9,22 +9,4 @@ package org.vitrivr.cottontail.database.queries.planning.nodes.physical
 abstract class NullaryPhysicalNodeExpression : PhysicalNodeExpression() {
     /** The arity of the [NullaryPhysicalNodeExpression] is always on. */
     override val inputArity = 0
-
-    /** True, if this [NullaryPhysicalNodeExpression] can be partitioned, false otherwise. */
-    abstract val canBePartitioned: Boolean
-
-    /**
-     * Tries to create [p] partitions of this [NullaryPhysicalNodeExpression] if possible. If the implementing
-     * [NullaryPhysicalNodeExpression] returns true for [canBePartitioned], then this method is expected
-     * to return a result, even if the number of partitions returned my be lower than [p] or even one (which
-     * means that no partitioning took place).
-     *
-     * If [canBePartitioned] returns false, this method is expected to throw a [IllegalStateException].
-     *
-     * @param p The desired number of partitions.
-     * @return Array of [NullaryPhysicalNodeExpression]s.
-     *
-     * @throws IllegalStateException If this [NullaryPhysicalNodeExpression] cannot be partitioned.
-     */
-    abstract fun partition(p: Int): List<NullaryPhysicalNodeExpression>
 }
--- src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/nodes/physical/UnaryPhysicalNodeExpression.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/nodes/physical/UnaryPhysicalNodeExpression.kt
@@ -10,6 +10,25 @@ abstract class UnaryPhysicalNodeExpression : PhysicalNodeExpression() {
     /** The arity of the [UnaryPhysicalNodeExpression] is always on. */
     override val inputArity = 1
 
+    /** [UnaryPhysicalNodeExpression] can be partitioned, if its parent can be partitioned. */
+    override val canBePartitioned: Boolean
+        get() = this.input.canBePartitioned
+
+    /**
+     * Tries to create [p] partitions of this [UnaryPhysicalNodeExpression], which is possible
+     * as long as all upstream [PhysicalNodeExpression]s can be partitioned.
+     *
+     * @param p The desired number of partitions.
+     * @return Array of [PhysicalNodeExpression]s.
+     *
+     * @throws IllegalStateException If this [PhysicalNodeExpression] cannot be partitioned.
+     */
+    override fun partition(p: Int): List<PhysicalNodeExpression> = this.input.partition(p).map {
+        val copy = this.copy()
+        copy.addInput(it)
+        copy
+    }
+
     /** Reference to the input [PhysicalNodeExpression]. */
     val input: PhysicalNodeExpression
         get() {
