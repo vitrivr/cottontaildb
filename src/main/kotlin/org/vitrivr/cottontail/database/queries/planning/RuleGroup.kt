dc8fa0df0d6a9149f75e5da4d53dd622f1affc2f
Ralph Gasser <rg@pontius.ch>
2020-11-03 06:44:46 +0100

    Adjusted CottontailQueryPlanner.

--- src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/RuleGroup.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/RuleGroup.kt
@@ -4,26 +4,29 @@ import org.vitrivr.cottontail.database.queries.planning.nodes.interfaces.NodeExp
 import org.vitrivr.cottontail.database.queries.planning.nodes.interfaces.RewriteRule
 
 /**
+ * A [RuleGroup] is a collection of [RewriteRule]s that can be applied to a [NodeExpression].
  *
  * @author Ralph Gasser
- * @version 1.0
+ * @version 1.0.1
  */
-data class RuleShuttle(val rules: Collection<RewriteRule>) {
+data class RuleGroup(val rules: Collection<RewriteRule>) {
     /**
-     * Apples the rules contained in this [RuleShuttle] to the given [NodeExpression]. If any rule
+     * Apples the rules contained in this [RuleGroup] to the given [NodeExpression]. If any rule
      * produces a result, it is added to the list of candidates
      *
      * @param expression The [NodeExpression] to apply rules to.
-     * @param candidates The candidates to add transformed [NodeExpression]s to.
+     * @return List of candidate [NodeExpression]s.
      */
-    fun apply(expression: NodeExpression, candidates: MutableList<NodeExpression>) {
+    fun apply(expression: NodeExpression): List<NodeExpression> {
+        val candidates = mutableListOf<NodeExpression>()
         for (rule in this.rules) {
             if (rule.canBeApplied(expression)) {
                 val result = rule.apply(expression)
                 if (result != null) {
-                    candidates.add(result.root)
+                    candidates.add(result)
                 }
             }
         }
+        return candidates
     }
 }
