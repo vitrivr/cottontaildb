969b0631b1092daaee60b5d24cb2db2cc455fc42
Ralph Gasser <rg@pontius.ch>
2020-11-03 06:36:10 +0100

    Fixed issue in EntityScanLogicalNodeExpression's copy() operation.

--- src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/nodes/logical/sources/EntityScanLogicalNodeExpression.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/nodes/logical/sources/EntityScanLogicalNodeExpression.kt
@@ -8,7 +8,7 @@ import org.vitrivr.cottontail.model.basics.ColumnDef
  * A [NullaryLogicalNodeExpression] that formalizes the scan of a physical [Entity] in Cottontail DB.
  *
  * @author Ralph Gasser
- * @version 1.0
+ * @version 1.0.0
  */
 class EntityScanLogicalNodeExpression(val entity: Entity, val columns: Array<ColumnDef<*>> = entity.allColumns().toTypedArray()): NullaryLogicalNodeExpression() {
 
@@ -17,5 +17,5 @@ class EntityScanLogicalNodeExpression(val entity: Entity, val columns: Array<Col
      *
      * @return Copy of this [EntityScanLogicalNodeExpression]
      */
-    override fun copy(): EntityScanLogicalNodeExpression = EntityScanLogicalNodeExpression(this.entity)
+    override fun copy(): EntityScanLogicalNodeExpression = EntityScanLogicalNodeExpression(this.entity, this.columns)
 }
