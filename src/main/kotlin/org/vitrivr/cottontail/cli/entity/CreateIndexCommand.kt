9b510f59cf772818ff153ab093f28561cabbc8a1
Loris Sauter <loris.sauter@unibas.ch>
2020-12-02 14:22:11 +0100

    Ad #35 added rebuild-after-creation for indices

--- src/main/kotlin/org/vitrivr/cottontail/cli/entity/CreateIndexCommand.kt
+++ src/main/kotlin/org/vitrivr/cottontail/cli/entity/CreateIndexCommand.kt
@@ -24,7 +24,7 @@ class CreateIndexCommand(
         private val ddlStub: CottonDDLGrpc.CottonDDLBlockingStub)
     : AbstractEntityCommand(
         name = "create-index",
-        help = "Creates an index on the given entity. Usage: entity createIndex <schema>.<entity> <column> <index>") {
+        help = "Creates an index on the given entity and rebuilds the newly created index. Usage: entity createIndex <schema>.<entity> <column> <index>") {
 
     private val attribute by argument(name="column", help="The column name to create the index for")
     private val index by argument(name="index", help = "The index to create").enum<CottontailGrpc.IndexType>()
@@ -47,8 +47,11 @@ class CreateIndexCommand(
         val status = measureTimedValue {
             ddlStub.createIndex(idxMsg)
         }
+        val rebuilt = measureTimedValue {
+            ddlStub.rebuildIndex(idx);
+        }
         if(status.value.success){
-            println("Successfully created index (in ${status.duration})")
+            println("Successfully created index (in ${status.duration}) and rebuilt in ${rebuilt.duration}")
         }else{
             println("Failed to create the index")
         }
