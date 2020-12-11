5a5679622f8144962781e37239697a8f10b36bfe
Ralph Gasser <rg@pontius.ch>
2020-12-11 19:37:22 +0100

    Added an Operator for parallel execution of filters (only for full table scan).
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/nodes/physical/predicates/FilterPhysicalNodeExpression.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/nodes/physical/predicates/FilterPhysicalNodeExpression.kt
@@ -4,22 +4,37 @@ import org.vitrivr.cottontail.database.queries.components.BooleanPredicate
 import org.vitrivr.cottontail.database.queries.planning.cost.Cost
 import org.vitrivr.cottontail.database.queries.planning.nodes.physical.UnaryPhysicalNodeExpression
 import org.vitrivr.cottontail.execution.ExecutionEngine
+import org.vitrivr.cottontail.execution.operators.basics.Operator
 import org.vitrivr.cottontail.execution.operators.predicates.FilterOperator
+import org.vitrivr.cottontail.execution.operators.predicates.ParallelFilterOperator
 
 /**
  * A [UnaryPhysicalNodeExpression] that represents application of a [BooleanPredicate] on some intermediate result.
  *
  * @author Ralph Gasser
- * @version 1.0
+ * @version 1.0.1
  */
 class FilterPhysicalNodeExpression(val predicate: BooleanPredicate, val selectivity: Float = Cost.COST_DEFAULT_SELECTIVITY) : UnaryPhysicalNodeExpression() {
+
+    companion object {
+        const val MAX_PARALLELISATION = 4
+    }
+
 
     override val outputSize: Long
         get() = (this.input.outputSize * this.selectivity).toLong()
 
     override val cost: Cost
-        get() = Cost(cpu = this.input.outputSize * this.predicate.cost)
+        get() = Cost(cpu = this.input.outputSize * this.predicate.cost * Cost.COST_MEMORY_ACCESS)
 
     override fun copy() = FilterPhysicalNodeExpression(this.predicate, this.selectivity)
-    override fun toOperator(context: ExecutionEngine.ExecutionContext) = FilterOperator(this.input.toOperator(context), context, this.predicate)
+    override fun toOperator(engine: ExecutionEngine): Operator {
+        val parallelisation = Integer.min(this.cost.parallelisation(), MAX_PARALLELISATION)
+        return if (parallelisation > 1) {
+            val operators = this.input.partition(parallelisation).map { it.toOperator(engine) }
+            ParallelFilterOperator(operators, this.predicate)
+        } else {
+            FilterOperator(this.input.toOperator(engine), this.predicate)
+        }
+    }
 }
