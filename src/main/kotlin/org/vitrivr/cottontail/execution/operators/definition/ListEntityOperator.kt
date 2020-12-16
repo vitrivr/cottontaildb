9a7df2600422f15c20955878444eeb9e2c34b5b0
Ralph Gasser <rg@pontius.ch>
2020-12-16 12:48:56 +0100

    Fixed bug that would locks to be retained even after completion of transaction.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/execution/operators/definition/ListEntityOperator.kt
+++ src/main/kotlin/org/vitrivr/cottontail/execution/operators/definition/ListEntityOperator.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.vitrivr.cottontail.database.catalogue.Catalogue
 import org.vitrivr.cottontail.database.catalogue.CatalogueTx
+import org.vitrivr.cottontail.database.schema.SchemaTx
 import org.vitrivr.cottontail.execution.TransactionContext
 import org.vitrivr.cottontail.execution.operators.basics.Operator
 import org.vitrivr.cottontail.model.basics.ColumnDef
@@ -35,7 +36,7 @@ class ListEntityOperator(val catalogue: Catalogue, val schema: Name.SchemaName? 
         }
         return flow {
             for (schema in schemas) {
-                val schemaTxn = txn.schemaForName(schema).newTx(context)
+                val schemaTxn = context.getTx(txn.schemaForName(schema)) as SchemaTx
                 for (entity in schemaTxn.listEntities()) {
                     emit(StandaloneRecord(0L, this@ListEntityOperator.columns, arrayOf(StringValue(entity.toString()), StringValue("ENTITY"))))
                 }
