94ec2db3aaaea514fac7810666f009984dcc7f80
Ralph Gasser <rg@pontius.ch>
2021-01-06 15:13:02 +0100

    Fixed naming issue in DropIndexCommand.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/cli/entity/DropIndexCommand.kt
+++ src/main/kotlin/org/vitrivr/cottontail/cli/entity/DropIndexCommand.kt
@@ -4,7 +4,6 @@ import com.github.ajalt.clikt.parameters.arguments.argument
 import io.grpc.StatusException
 import org.vitrivr.cottontail.grpc.CottontailGrpc
 import org.vitrivr.cottontail.grpc.DDLGrpc
-import org.vitrivr.cottontail.model.basics.Name
 import org.vitrivr.cottontail.server.grpc.helper.proto
 import org.vitrivr.cottontail.utilities.output.TabulationUtilities
 import kotlin.time.ExperimentalTime
@@ -17,12 +16,12 @@ import kotlin.time.measureTimedValue
  * @version 1.0.1
  */
 @ExperimentalTime
-class DropIndexCommand(private val ddlStub: DDLGrpc.DDLBlockingStub) : AbstractEntityCommand(name="drop-index", help="Drops the index on an entity. Usage: entity drop-index <schema>.<entity> <index>") {
+class DropIndexCommand(private val ddlStub: DDLGrpc.DDLBlockingStub) : AbstractEntityCommand(name = "drop-index", help = "Drops the index on an entity. Usage: entity drop-index <schema>.<entity> <index>") {
 
-    val name: String by argument(name="name", help = "The name of the index. Ideally from a previous list-indices call")
+    val name: String by argument(name = "name", help = "The name of the index. Ideally from a previous list-indices call")
 
     override fun exec() {
-        val index = Name.IndexName(this.name)
+        val index = this.entityName.index(name)
         try {
             val timedTable = measureTimedValue {
                 TabulationUtilities.tabulate(this.ddlStub.dropIndex(CottontailGrpc.DropIndexMessage.newBuilder().setIndex(index.proto()).build()))
