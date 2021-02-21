3a1e3e35c60296e299d0e95011e82d57c13943d8
Ralph Gasser <rg@pontius.ch>
2021-02-21 20:07:43 +0100

    Replaced SortOrder wrap by an operator function.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/database/queries/sort/SortOrder.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/queries/sort/SortOrder.kt
@@ -1,25 +1,17 @@
 package org.vitrivr.cottontail.database.queries.sort
 
-import org.vitrivr.cottontail.model.basics.Record
-
 /**
  * Enumeration that describes the sort order.
  *
  * @author Ralph Gasser
  * @version 1.0.0
  */
-enum class SortOrder {
-    ASCENDING,
-    DESCENDING;
+enum class SortOrder(val sign: Int) {
+    ASCENDING(1),
+    DESCENDING(-1);
 
     /**
-     * Wraps the given [Comparator] based on the [SortOrder].
-     *
-     * @param comparator The [Comparator] to wrap.
-     * @return Wrapped [Comparator].
+     * Allows for multiplication with other int.
      */
-    fun wrap(comparator: Comparator<Record>): Comparator<Record> = when (this) {
-        ASCENDING -> comparator
-        DESCENDING -> Comparator { r1, r2 -> -comparator.compare(r1, r2) }
-    }
+    operator fun times(other: Int): Int = this.sign * other
 }
