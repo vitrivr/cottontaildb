91edf637dc73dd4599b81956ea8b51882d000072
Ralph Gasser <rg@pontius.ch>
2021-03-12 10:03:21 +0100

    Slight change to how VAFSignatures are serialized.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/database/index/va/signature/VAFSignature.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/index/va/signature/VAFSignature.kt
@@ -7,24 +7,23 @@ import org.mapdb.DataOutput2
  * A fixed length [VAFSignature] used for vector approximation.
  *
  * @author Gabriel Zihlmann & Ralph Gasser
- * @version 1.0.0
+ * @version 1.0.1
  */
 data class VAFSignature(val tupleId: Long, val cells: IntArray) {
     companion object Serializer : org.mapdb.Serializer<VAFSignature> {
         override fun serialize(out: DataOutput2, value: VAFSignature) {
-            out.writeLong(value.tupleId)
+            out.packLong(value.tupleId)
             out.packInt(value.cells.size)
             for (c in value.cells) {
-                out.writeByte(c)
+                out.packInt(c)
             }
         }
 
         override fun deserialize(input: DataInput2, available: Int) = VAFSignature(
-            input.readLong(),
-            IntArray(input.unpackInt()) { input.readUnsignedByte() }
+            input.unpackLong(),
+            IntArray(input.unpackInt()) { input.unpackInt() }
         )
     }
-
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -36,7 +35,6 @@ data class VAFSignature(val tupleId: Long, val cells: IntArray) {
 
         return true
     }
-
     override fun hashCode(): Int {
         var result = tupleId.hashCode()
         result = 31 * result + cells.contentHashCode()
