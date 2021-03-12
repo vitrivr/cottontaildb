eddc6ba8b84364e576923a1b86cf913aa1515d7e
Ralph Gasser <rg@pontius.ch>
2021-03-12 13:09:06 +0100

    HeapSelection is now thread-safe.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/math/knn/selection/HeapSelection.kt
+++ src/main/kotlin/org/vitrivr/cottontail/math/knn/selection/HeapSelection.kt
@@ -1,9 +1,13 @@
 package org.vitrivr.cottontail.math.knn.selection
 
 import it.unimi.dsi.fastutil.objects.ObjectBigArrayBigList
+import org.vitrivr.cottontail.utilities.extensions.read
+import org.vitrivr.cottontail.utilities.extensions.write
+import java.util.concurrent.locks.StampedLock
 import kotlin.math.min
 
 /**
+ * A data structure used for heap based sorting.
  *
  * @author Ralph Gasser
  * @version 1.0.0
@@ -13,24 +17,29 @@ class HeapSelection<T>(val k: Long, val comparator: Comparator<T>) {
     /** The [ArrayList] containing the heap for this [HeapSelection]. */
     private val heap = ObjectBigArrayBigList<T>(this.k)
 
+    /** A lock that mediates access to this [HeapSelection]. */
+    private val lock = StampedLock()
+
     /** Indicates whether this [MinHeapSelection] is currently sorted or not. */
+    @Volatile
     var sorted: Boolean = true
         private set
 
     /** Number of items that have been added to this [HeapSelection] so far. */
+    @Volatile
     var added: Long = 0
         private set
 
     /** Returns the size of this [HeapSelection], i.e., the number of items contained in the heap. */
     val size: Long
-        get() = this.heap.size64()
+        get() = this.lock.read { this.heap.size64() }
 
     /**
-     * Adds a new element to this [MinHeapSelection].
+     * Adds a new element to this [HeapSelection].
      *
-     * @param element The element to add to this [MinHeapSelection].
+     * @param element The element to add to this [HeapSelection].
      */
-    fun offer(element: T) {
+    fun offer(element: T) = this.lock.write {
         if ((this.added++) < this.k) {
             this.sorted = false
             this.heap.add(element)
@@ -50,7 +59,7 @@ class HeapSelection<T>(val k: Long, val comparator: Comparator<T>) {
      *
      * @return Largest value seen so far.
      */
-    fun peek(): T? = this.heap.firstOrNull()
+    fun peek(): T? = this.lock.read { this.heap.firstOrNull() }
 
     /**
      * Returns the i-*th* retained value seen  by this [MinHeapSelection]. i = 0 returns the
@@ -60,7 +69,7 @@ class HeapSelection<T>(val k: Long, val comparator: Comparator<T>) {
      * @param i The index of the value to return
      * @return The i-th smallest value retained by this [MinHeapSelection]
      */
-    operator fun get(i: Long): T {
+    operator fun get(i: Long): T = this.lock.read {
         val maxIdx = this.heap.size64() - 1
         require(i <= maxIdx) { "Index $i is out of bounds for this HeapSelect." }
 
@@ -126,7 +135,7 @@ class HeapSelection<T>(val k: Long, val comparator: Comparator<T>) {
     }
 
     /**
-     *
+     * Heapifies this [HeapSelection].
      */
     private fun heapify() {
         val n = this.heap.size64()
