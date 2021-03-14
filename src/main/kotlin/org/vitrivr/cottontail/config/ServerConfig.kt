cc120540ad18fbf3005a9624be80a9746589bdd1
Ralph Gasser <rg@pontius.ch>
2021-03-14 12:57:01 +0100

    Updated ServerConfig
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/config/ServerConfig.kt
+++ src/main/kotlin/org/vitrivr/cottontail/config/ServerConfig.kt
@@ -7,28 +7,21 @@ import java.nio.file.Path
 /**
  * Config for Cottontail DB's gRPC server.
  *
- *
- * @param port The port under which Cottontail DB demon should be listening for calls. Defaults to 1865
- * @param keepAliveTime The number of milliseconds to wait before decommissioning unused threads.
- * @param certFile Path to the certificate file used for TLS.
- * @param privateKey Path to the private key used for TLS.
- *
  * @author Ralph Gasser
- * @version 1.0.2
+ * @version 1.0.3
  */
 @Serializable
 data class ServerConfig(
-    /** The port under which the server should be reachable. */
+    /** The port under which the gRPC server should be reachable. */
     val port: Int = 1865,
 
-    /** Keep-alive time for request. */
-    val keepAliveTime: Long = 500,
+    /** Optional path to certificate file (for TLS). */
+    @Serializable(with = PathSerializer::class)
+    val certFile: Path? = null,
 
-    /** Optional path to certificate file (for SSL). */
-    @Serializable(with = PathSerializer::class) val certFile: Path? = null,
-
-    /** Optional path to private key file (fpr SSL). */
-    @Serializable(with = PathSerializer::class) val privateKey: Path? = null
+    /** Optional path to private key file (for TLS). */
+    @Serializable(with = PathSerializer::class)
+    val privateKey: Path? = null
 ) {
 
     /**
