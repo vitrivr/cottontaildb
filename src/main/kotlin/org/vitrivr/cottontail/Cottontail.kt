4ef37750070176656c63b87f0469b97466f4bbd2
Ralph Gasser <rg@pontius.ch>
2021-03-17 15:19:57 +0100

    Fixed issue during startup that would occur if folder / catalogue does not exists.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/Cottontail.kt
+++ src/main/kotlin/org/vitrivr/cottontail/Cottontail.kt
@@ -50,9 +50,8 @@ fun main(args: Array<String>) {
     /* Load config file and start Cottontail DB. */
     Files.newBufferedReader(configPath).use { reader ->
         val config = Json.decodeFromString(Config.serializer(), reader.readText())
-        if (!Files.isDirectory(config.root)) {
-            System.err.println("Specified Cottontail DB data folder file $${config.root} does not exist. Cottontail DB will shut down.")
-            exitProcess(1)
+        if (!Files.exists(config.root)) {
+            Files.createDirectories(config.root)
         }
         try {
             standalone(config)
@@ -78,10 +77,7 @@ fun standalone(config: Config) {
 
     /* Check catalogue version. */
     val detected = VersionProber(config).probe(config.root)
-    if (detected == DBOVersion.UNDEFINED) {
-        System.err.println("Folder does not seem to be a valid Cottontail DB data folder (detected = $detected, path = ${config.root}.")
-        exitProcess(1)
-    } else if (detected != VersionProber.EXPECTED) {
+    if (detected != VersionProber.EXPECTED && detected != DBOVersion.UNDEFINED) {
         System.err.println("Version mismatch: Trying to open an incompatible Cottontail DB catalogue version. Run system migration to upgrade catalogue (detected = $detected, expected = ${VersionProber.EXPECTED}, path = ${config.root}).")
         exitProcess(1)
     }
@@ -92,7 +88,7 @@ fun standalone(config: Config) {
     /* Start gRPC Server and print message. */
     val server = CottontailGrpcServer(config, catalogue)
     server.start()
-    println("Cottontail DB server is up and running at port ${config.server.port}! Hop along... (catalogue: $detected, pid: ${ProcessHandle.current().pid()})")
+    println("Cottontail DB server is up and running at port ${config.server.port}! Hop along... (catalogue: ${catalogue.version}, pid: ${ProcessHandle.current().pid()})")
 
     /* Start CLI (if configured). */
     if (config.cli) {
