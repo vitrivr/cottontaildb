2664f93dec59ebdd45a6f0363239e1d5ea20678c
Ralph Gasser <rg@pontius.ch>
2021-03-21 14:32:17 +0100

    Unknown errors that occur during query execution are now propagated.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/execution/TransactionManager.kt
+++ src/main/kotlin/org/vitrivr/cottontail/execution/TransactionManager.kt
@@ -175,7 +175,7 @@ class TransactionManager(private val executor: ThreadPoolExecutor, transactionTa
                     throw e
                 } catch (e: Throwable) {
                     this@Transaction.state = TransactionStatus.ERROR
-                    throw ExecutionException("Unhandled exception during execution of transaction ${this@Transaction.txId}: ${e.message}")
+                    throw e
                 }
             }
         }
