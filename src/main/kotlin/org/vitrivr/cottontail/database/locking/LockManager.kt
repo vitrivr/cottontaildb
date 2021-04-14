79906c44ed0896e2f68c045e2981852722c9d0e1
Ralph Gasser <rg@pontius.ch>
2021-04-14 17:09:20 +0200

    Fixed a race condition on LockManager that caused locks not being released properly.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/database/locking/LockManager.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/locking/LockManager.kt
@@ -3,7 +3,6 @@ package org.vitrivr.cottontail.database.locking
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
 import org.vitrivr.cottontail.database.general.DBO
 import org.vitrivr.cottontail.model.basics.Name
-import java.util.*
 
 /**
  * A [LockManager] implementation that allows for management of [Lock]s on [DBO]s.
@@ -16,7 +15,7 @@ import java.util.*
 class LockManager {
 
     /** List of all [Lock]s managed by this [LockManager]. */
-    private val locks = Collections.synchronizedMap(Object2ObjectOpenHashMap<DBO, Lock>())
+    private val locks = Object2ObjectOpenHashMap<DBO, Lock>()
 
     /** The [WaitForGraph] data structure used to detect deadlock situations. */
     private val waitForGraph: WaitForGraph = WaitForGraph()
@@ -35,7 +34,7 @@ class LockManager {
      * @param dbo [DBO] of the object to acquire a lock on.
      * @param mode The [LockMode]
      */
-    fun lock(txn: LockHolder, dbo: DBO, mode: LockMode) {
+    fun lock(txn: LockHolder, dbo: DBO, mode: LockMode) = synchronized(dbo) {
         require(mode != LockMode.NO_LOCK) { "Cannot acquire a lock of mode $mode; try LockManager.release()." }
         val lockOn = lockOn(txn, dbo)
         if (lockOn === mode) {
@@ -51,7 +50,6 @@ class LockManager {
             lock.acquire(txn, mode)
             txn.addLock(lock)
         }
-        return
     }
 
     /**
@@ -60,7 +58,7 @@ class LockManager {
      * @param txn [LockHolder] to release the lock for.
      * @param dbo [DBO] of the object to release the lock on.
      */
-    fun unlock(txn: LockHolder, dbo: DBO) {
+    fun unlock(txn: LockHolder, dbo: DBO) = synchronized(dbo) {
         this.locks.computeIfPresent(dbo) { _, lock ->
             lock.release(txn)
             txn.removeLock(lock)
