4690d4a405a3eb3256883a6405a2cb636f732e38
Ralph Gasser <rg@pontius.ch>
2021-05-03 11:32:36 +0200

    Fixed a LockManager locking issue.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- src/main/kotlin/org/vitrivr/cottontail/database/locking/LockManager.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/locking/LockManager.kt
@@ -1,11 +1,9 @@
 package org.vitrivr.cottontail.database.locking
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap
+import it.unimi.dsi.fastutil.objects.Object2ObjectMaps
 import org.vitrivr.cottontail.database.general.DBO
 import org.vitrivr.cottontail.model.basics.Name
-import org.vitrivr.cottontail.utilities.extensions.read
-import org.vitrivr.cottontail.utilities.extensions.write
-import java.util.concurrent.locks.StampedLock
 
 /**
  * A [LockManager] implementation that allows for management of [Lock]s on [DBO]s.
@@ -18,22 +16,18 @@ import java.util.concurrent.locks.StampedLock
 class LockManager<T> {
 
     /** List of all [Lock]s managed by this [LockManager]. */
-    private val locks = Object2ObjectLinkedOpenHashMap<T, Lock<T>>()
+    private val locks = Object2ObjectMaps.synchronize(Object2ObjectLinkedOpenHashMap<T, Lock<T>>())
 
     /** The [WaitForGraph] data structure used to detect deadlock situations. */
     private val waitForGraph: WaitForGraph = WaitForGraph()
 
-    /** [StampedLock] used to mediate access to [LockManager]. */
-    private val accessLock = StampedLock()
 
     /**
      * Returns an list all [T] that are currently locked.
      *
      * @return List of all [T]s that are currently locked.
      */
-    fun allLocks(): List<Pair<T, Lock<T>>> = this.accessLock.read {
-        this.locks.map { v -> v.key to v.value }
-    }
+    fun allLocks(): List<Pair<T, Lock<T>>> = this.locks.map { v -> v.key to v.value }
 
     /**
      * Tries to acquire a lock on [Name] for the given [LockHolder].
@@ -42,7 +36,7 @@ class LockManager<T> {
      * @param obj Object [T] to acquire a lock on.
      * @param mode The [LockMode]
      */
-    fun lock(txn: LockHolder<T>, obj: T, mode: LockMode) = this.accessLock.write {
+    fun lock(txn: LockHolder<T>, obj: T, mode: LockMode) = synchronized(obj as Any) {
         require(mode != LockMode.NO_LOCK) { "Cannot acquire a lock of mode $mode; try LockManager.release()." }
         val lockOn = txn.lockOn(obj)
         if (lockOn === mode) {
@@ -66,7 +60,7 @@ class LockManager<T> {
      * @param txn [LockHolder] to release the lock for.
      * @param obj Object [T] to release the lock on.
      */
-    fun unlock(txn: LockHolder<T>, obj: T) = this.accessLock.write {
+    fun unlock(txn: LockHolder<T>, obj: T) = synchronized(obj as Any) {
         this.locks.computeIfPresent(obj) { _, lock ->
             lock.release(txn)
             txn.removeLock(obj)
