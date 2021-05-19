5a25ee9d67771b1dc10d3aa26aca029e8dbff992
Ralph Gasser <rg@pontius.ch>
2021-05-19 06:01:37 +0200

    Corrected DeferFetchOnFetchRewriteRule and added unit test for it.

--- src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/rules/logical/DeferFetchOnFetchRewriteRule.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/queries/planning/rules/logical/DeferFetchOnFetchRewriteRule.kt
@@ -20,54 +20,53 @@ object DeferFetchOnFetchRewriteRule : RewriteRule {
         if (node is FetchLogicalOperatorNode) {
             val originalColumns = node.fetch
             val originalGroupId = node.groupId
-
             var copy = node.input!!.copyWithInputs()
 
             /* Traverse tree and push down FetchLogicalOperatorNode. */
             var next: OperatorNode.Logical? = node.output
             while (next != null && next.groupId == originalGroupId) {
-
                 /* Append FetchLogicalOperatorNode for columns required by next element. */
                 val required = originalColumns.filter { it in next!!.requires }.toTypedArray()
-                if (required.isNotEmpty()) {
+                if (required.isEmpty()) {
+                    next = next.output /* No requirement necessary. */
+                } else if (required.size == originalColumns.size) {
+                    break /* No deferral possible; abort. */
+                } else {
                     copy = FetchLogicalOperatorNode(copy, node.entity, required)
-                }
-
-                /* Append next element. */
-                when (next) {
-                    is UnaryLogicalOperatorNode -> {
-                        val p = next.copy()
-                        p.input = copy
-                        copy = p
-                    }
-                    is BinaryLogicalOperatorNode -> {
-                        val p = next.copy()
-                        p.left = copy
-                        p.right = next.right?.copyWithInputs()
-                        copy = p
-                    }
-                    is NAryLogicalOperatorNode -> {
-                        val p = next.copy()
-                        p.addInput(copy)
-                        for (it in next.inputs.drop(1)) {
-                            p.addInput(it.copyWithInputs())
+                    /* Append next element. */
+                    when (next) {
+                        is UnaryLogicalOperatorNode -> {
+                            val p = next.copy()
+                            p.input = copy
+                            copy = p
                         }
-                        copy = p
+                        is BinaryLogicalOperatorNode -> {
+                            val p = next.copy()
+                            p.left = copy
+                            p.right = next.right?.copyWithInputs()
+                            copy = p
+                        }
+                        is NAryLogicalOperatorNode -> {
+                            val p = next.copy()
+                            p.addInput(copy)
+                            for (it in next.inputs.drop(1)) {
+                                p.addInput(it.copyWithInputs())
+                            }
+                            copy = p
+                        }
+                        else -> throw IllegalArgumentException("Encountered unsupported node during execution of DeferredFetchRewriteRule.")
                     }
-                    else -> throw IllegalArgumentException("Encountered unsupported node during execution of DeferredFetchRewriteRule.")
-                }
 
-                /* Append FetchLogicalOperatorNode for columns not required by next element and return. */
-                if (required.isNotEmpty()) {
-                    val defer = originalColumns.filter { it !in required }.toTypedArray()
-                    if (defer.isNotEmpty() && next.output != null) {
-                        copy = FetchLogicalOperatorNode(copy, node.entity, defer)
+                    /* Append FetchLogicalOperatorNode for columns not required by next element and return. */
+                    if (required.isNotEmpty()) {
+                        val defer = originalColumns.filter { it !in required }.toTypedArray()
+                        if (defer.isNotEmpty() && next.output != null) {
+                            copy = FetchLogicalOperatorNode(copy, node.entity, defer)
+                        }
+                        return next.output?.copyWithOutput(copy) ?: copy
                     }
-                    return next.output?.copyWithOutput(copy) ?: copy
-                }
 
-                /* Move down the tree. */
-                next = next.output
+                }
             }
         }
         return null
