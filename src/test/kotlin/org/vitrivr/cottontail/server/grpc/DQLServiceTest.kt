e86734f27a594dcea3e0f44065eec631f82f0562
Ralph Gasser <ralph.gasser@unibas.ch>
2021-07-02 10:27:57 +0200

    Refactored DQLServiceTests and associated constants and added two test cases for BETWEEN and BETWEEN with an (should address certain bug encountered in Cineast).

--- src/test/kotlin/org/vitrivr/cottontail/server/grpc/DQLServiceTest.kt
+++ src/test/kotlin/org/vitrivr/cottontail/server/grpc/DQLServiceTest.kt
@@ -4,14 +4,12 @@ import io.grpc.ManagedChannel
 import io.grpc.netty.NettyChannelBuilder
 import org.junit.jupiter.api.*
 import org.vitrivr.cottontail.TestConstants
-import org.vitrivr.cottontail.TestConstants.STRING_COLUMN_NAME
-import org.vitrivr.cottontail.TestConstants.TEST_ENTITY_FQN_INPUT
-import org.vitrivr.cottontail.TestConstants.TEST_ENTITY_TUPLE_COUNT
-import org.vitrivr.cottontail.TestConstants.TEST_VECTOR_ENTITY_FQN_INPUT
-import org.vitrivr.cottontail.TestConstants.TWOD_COLUMN_NAME
 import org.vitrivr.cottontail.client.language.dql.Query
+import org.vitrivr.cottontail.client.language.extensions.And
+import org.vitrivr.cottontail.client.language.extensions.Literal
 import org.vitrivr.cottontail.client.stub.SimpleClient
 import org.vitrivr.cottontail.embedded
+import kotlin.random.Random
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
@@ -22,7 +20,6 @@ class DQLServiceTest {
     private lateinit var channel: ManagedChannel
     private lateinit var embedded: CottontailGrpcServer
 
-
     @BeforeAll
     fun startCottontail() {
         this.embedded = embedded(TestConstants.testConfig())
@@ -31,17 +28,17 @@ class DQLServiceTest {
         this.channel = builder.build()
         this.client = SimpleClient(this.channel)
         assert(client.ping())
-        dropTestSchema(client)
-        createTestSchema(client)
-        createTestVectorEntity(client)
-        createTestEntity(client)
-        populateTestEntity(client)
-        populateVectorEntity(client)
+        GrpcTestUtils.dropTestSchema(client)
+        GrpcTestUtils.createTestSchema(client)
+        GrpcTestUtils. createTestVectorEntity(client)
+        GrpcTestUtils.createTestEntity(client)
+        GrpcTestUtils.populateTestEntity(client)
+        GrpcTestUtils.populateVectorEntity(client)
     }
 
     @AfterAll
     fun cleanup() {
-        dropTestSchema(client)
+        GrpcTestUtils.dropTestSchema(client)
         this.embedded.stop()
     }
 
@@ -62,32 +59,64 @@ class DQLServiceTest {
 
     @Test
     fun count() {
-        val countQuery = Query(TEST_ENTITY_FQN_INPUT).count()
+        val countQuery = Query(GrpcTestUtils.TEST_ENTITY_FQN_INPUT).count()
         val count = client.query(countQuery).next()
-        assert(count.asLong(0)!! == TEST_ENTITY_TUPLE_COUNT)
+        assert(count.asLong(0)!! == GrpcTestUtils.TEST_ENTITY_TUPLE_COUNT)
     }
 
     @Test
     fun queryColumn() {
-        val query = Query().from(TEST_ENTITY_FQN_INPUT).select(STRING_COLUMN_NAME)
+        val query = Query().from(GrpcTestUtils.TEST_ENTITY_FQN_INPUT).select(GrpcTestUtils.STRING_COLUMN_NAME)
         val result = client.query(query)
         assert(result.numberOfColumns == 1)
         val el = result.next()
-        assert(!el.asString(STRING_COLUMN_NAME).equals(""))
+        assert(!el.asString(GrpcTestUtils.STRING_COLUMN_NAME).equals(""))
+    }
+
+    @Test
+    fun queryBetween() {
+        val random = Random.Default
+        val lb = random.nextInt(98)
+        val ub = random.nextInt(lb+1, 100)
+        val query = Query().from(GrpcTestUtils.TEST_ENTITY_FQN_INPUT)
+            .select(GrpcTestUtils.STRING_COLUMN_NAME, GrpcTestUtils.INT_COLUMN_NAME)
+            .where(Literal(GrpcTestUtils.INT_COLUMN_NAME, "BETWEEN", lb, ub))
+        val result = client.query(query)
+        for (el in result) {
+            assert(el.asInt(GrpcTestUtils.INT_COLUMN_NAME)!! in lb..ub)
+        }
+    }
+
+    @Test
+    fun queryBetweenWithAnd() {
+        val random = Random.Default
+        val lb = random.nextInt(50)
+        val query = Query().from(GrpcTestUtils.TEST_ENTITY_FQN_INPUT)
+            .select(GrpcTestUtils.STRING_COLUMN_NAME, GrpcTestUtils.INT_COLUMN_NAME)
+            .where(
+                 And(
+                     Literal(GrpcTestUtils.INT_COLUMN_NAME, "BETWEEN", lb, lb+1),
+                     Literal(GrpcTestUtils.INT_COLUMN_NAME, "BETWEEN", lb+1, lb+2)
+                )
+            )
+        val result = client.query(query)
+        for (el in result) {
+            assert(el.asInt(GrpcTestUtils.INT_COLUMN_NAME)!! == lb + 1)
+        }
     }
 
     @Test
     fun queryColumnWithVector() {
-        val query = Query().from(TEST_VECTOR_ENTITY_FQN_INPUT).select(STRING_COLUMN_NAME)
+        val query = Query().from(GrpcTestUtils.TEST_VECTOR_ENTITY_FQN_INPUT).select(GrpcTestUtils.STRING_COLUMN_NAME)
         val result = client.query(query)
         assert(result.numberOfColumns == 1)
         val el = result.next()
-        assert(!el.asString(STRING_COLUMN_NAME).equals(""))
+        assert(!el.asString(GrpcTestUtils.STRING_COLUMN_NAME).equals(""))
     }
 
     @Test
     fun haversineDistance() {
-        val query = Query().from(TEST_VECTOR_ENTITY_FQN_INPUT).knn(TWOD_COLUMN_NAME, 2, "haversine", arrayOf(5f, 10f))
+        val query = Query().from(GrpcTestUtils.TEST_VECTOR_ENTITY_FQN_INPUT).knn(GrpcTestUtils.TWOD_COLUMN_NAME, 2, "haversine", arrayOf(5f, 10f))
         val result = client.query(query)
         val el = result.next()
         val distance = el.asDouble("distance")
