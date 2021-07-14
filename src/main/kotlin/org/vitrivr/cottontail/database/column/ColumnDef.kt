88ebbe6d96959b76fa547616f9793bc35f64fe93
Ralph Gasser <ralph.gasser@unibas.ch>
2021-07-14 11:59:51 +0200

    Minor optimisation of ColumnDef implementation.

--- src/main/kotlin/org/vitrivr/cottontail/database/column/ColumnDef.kt
+++ src/main/kotlin/org/vitrivr/cottontail/database/column/ColumnDef.kt
@@ -8,11 +8,11 @@ import org.vitrivr.cottontail.model.values.types.Value
 
 
 /**
- * A definition class for a Cottontail DB column be it in a DB or in-memory context
- * Specifies all the properties of such a and facilitates validation.
+ * A definition class for a Cottontail DB column be it in a persistent or in-memory context. Specifies all the properties
+ * of such and facilitates validation of [Value]s with respect to the [ColumnDef].
  *
  * @author Ralph Gasser
- * @version 1.5.0
+ * @version 1.5.1
  */
 data class ColumnDef<T : Value>(val name: Name.ColumnName, val type: Type<T>, val nullable: Boolean = true, val primary: Boolean = false) {
 
@@ -42,11 +42,11 @@ data class ColumnDef<T : Value>(val name: Name.ColumnName, val type: Type<T>, va
      * @param value The value that should be validated.
      * @return True if value passes validation, false otherwise.
      */
-    fun validate(value: Value?): Boolean {
-        return if (value != null) {
-            this.type.compatible(value)
-        } else this.nullable
-    }
+    fun validate(value: Value?): Boolean
+        = ((value == null && this.nullable) || (value != null && this.type.compatible(value)))
 
-    override fun toString(): String = "$name(type=$type, nullable=$nullable)"
+    /**
+     * Creates and returns a [String] representation of this [ColumnDef].
+     */
+    override fun toString(): String = "$name(type=$type, nullable=$nullable, primary=$primary)"
 }
