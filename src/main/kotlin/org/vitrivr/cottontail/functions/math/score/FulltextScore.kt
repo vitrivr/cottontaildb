70be83c6eca0c0b853347359c400ad3d8d836225
Ralph Gasser <rg@pontius.ch>
2021-10-03 17:03:47 +0200

    Fxied FulltextScore function.

--- src/main/kotlin/org/vitrivr/cottontail/functions/math/score/FulltextScore.kt
+++ src/main/kotlin/org/vitrivr/cottontail/functions/math/score/FulltextScore.kt
@@ -5,9 +5,8 @@ import org.vitrivr.cottontail.functions.basics.Function
 import org.vitrivr.cottontail.functions.basics.Signature
 import org.vitrivr.cottontail.model.basics.Name
 import org.vitrivr.cottontail.model.basics.Type
-import org.vitrivr.cottontail.model.values.StringValue
+import org.vitrivr.cottontail.model.values.DoubleValue
 import org.vitrivr.cottontail.model.values.types.Value
-import org.vitrivr.cottontail.model.values.types.VectorValue
 
 /**
  * This function is a mere placeholder used to express Lucene searches.
@@ -15,11 +14,11 @@ import org.vitrivr.cottontail.model.values.types.VectorValue
  * @author Ralph Gasser
  * @version 1.0.0
  */
-class FulltextScore<T : VectorValue<*>>: Function.Stateless<StringValue> {
-    override val signature: Signature.Closed<out StringValue>
-        = Signature.Closed(Name.FunctionName("fulltext"), *arrayOf(Argument.Typed(Type.String)), Type.String)
+object FulltextScore: Function.Stateless<DoubleValue> {
+    override val signature: Signature.Closed<out DoubleValue>
+        = Signature.Closed(Name.FunctionName("fulltext"), *arrayOf(Argument.Typed(Type.String), Argument.Typed(Type.String)), Type.Double)
     override val cost: Float = Float.MIN_VALUE
-    override fun invoke(vararg arguments: Value?): StringValue {
+    override fun invoke(vararg arguments: Value?): DoubleValue {
         throw UnsupportedOperationException("LuceneScore cannot be executed directly.")
     }
 }
