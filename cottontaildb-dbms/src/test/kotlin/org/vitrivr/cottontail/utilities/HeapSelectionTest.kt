0f768f596766d32a22664b85a0c1c51cf6af3050
Ralph Gasser <rg@pontius.ch>
2022-01-31 14:47:33 +0100

    Added few simple tests for verification of HeapSelection class.

---
+++ cottontaildb-dbms/src/test/kotlin/org/vitrivr/cottontail/utilities/HeapSelectionTest.kt
@@ -0,0 +1,75 @@
+package org.vitrivr.cottontail.utilities
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.vitrivr.cottontail.utilities.extensions.nextFloat
+import org.vitrivr.cottontail.utilities.selection.HeapSelection
+import java.lang.Float.max
+import java.lang.Float.min
+import java.util.*
+import kotlin.Comparator
+
+/**
+ * Simple test case for [HeapSelection] class, which is used when sorting and limiting data collections.
+ *
+ * @author Ralph Gasser
+ * @version 1.0.0
+ */
+class HeapSelectionTest {
+
+    /**
+     * Simple functional test of [HeapSelection] with [Float] value.
+     */
+    @Test
+    fun testMinWithSingleFloat() {
+        val selection = HeapSelection(1, Comparator<Float> { o1, o2 -> o1.compareTo(o2) })
+        val random = SplittableRandom()
+        var min = Float.MAX_VALUE
+        repeat(100000) {
+            val value = random.nextFloat()
+            selection.offer(value)
+            min = min(value, min)
+        }
+        Assertions.assertTrue(selection[0L] == min)
+    }
+
+    /**
+     * Simple functional test of [HeapSelection] with [Float] value.
+     */
+    @Test
+    fun testMaxWithSingleFloat() {
+        val selection = HeapSelection(1, Comparator<Float> { o1, o2 -> -o1.compareTo(o2) })
+        val random = SplittableRandom()
+        var max = Float.MIN_VALUE
+        repeat(100000) {
+            val value = random.nextFloat()
+            selection.offer(value)
+            max = max(value, max)
+        }
+        Assertions.assertTrue(selection[0L] == max)
+    }
+
+    /**
+     * Simple functional test of [HeapSelection] with [Int] values.
+     */
+    @Test
+    fun testWithIntAscending() {
+        val selection = HeapSelection(100, Comparator<Int> { o1, o2 -> o1.compareTo(o2) })
+        (0 until 100000).toList().shuffled().forEach { selection.offer(it) }
+        for (i in 0 until 100) {
+            Assertions.assertTrue(selection[i.toLong()] == i)
+        }
+    }
+
+    /**
+     * Simple functional test of [HeapSelection] with [Int] values.
+     */
+    @Test
+    fun testWithIntDescending() {
+        val selection = HeapSelection(100, Comparator<Int> { o1, o2 -> -o1.compareTo(o2) })
+        (0 until 100000).toList().shuffled().forEach { selection.offer(it) }
+        for (i in 0 until 100) {
+            Assertions.assertTrue(selection[i.toLong()] == 99999 - i)
+        }
+    }
+}
