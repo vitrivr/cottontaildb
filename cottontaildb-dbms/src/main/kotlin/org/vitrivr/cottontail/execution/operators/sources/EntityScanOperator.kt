adb10bbee15ab2b933d3113ca2351f08626e2071
Ralph Gasser <rg@pontius.ch>
2022-01-31 13:10:14 +0100

    EntityScanOperator now properly renames columns again.

--- cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/execution/operators/sources/EntityScanOperator.kt
+++ cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/execution/operators/sources/EntityScanOperator.kt
@@ -30,9 +30,11 @@ class EntityScanOperator(groupId: GroupId, val entity: EntityTx, val fetch: List
      */
     override fun toFlow(context: TransactionContext): Flow<Record> {
         val fetch = this.fetch.map { it.second }.toTypedArray()
+        val columns = this.fetch.map { it.first.column }.toTypedArray()
         return flow {
             for (record in this@EntityScanOperator.entity.scan(fetch, this@EntityScanOperator.partitionIndex, this@EntityScanOperator.partitions)) {
                 for (i in 0 until record.size) {
+                    record.columns[i] = columns[i] /* Rename columns. */
                     this@EntityScanOperator.fetch[i].first.update(record[i])
                 }
                 emit(record)
