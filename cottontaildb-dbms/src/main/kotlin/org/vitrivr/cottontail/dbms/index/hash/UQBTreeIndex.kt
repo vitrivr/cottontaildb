7ec61d2cf7d356d4c64e647bd970d3369c65b3c2
Ralph Gasser <rg@pontius.ch>
2022-03-21 15:10:28 +0100

    Fixed column access during rebuild.

--- cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/index/hash/UQBTreeIndex.kt
+++ cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/index/hash/UQBTreeIndex.kt
@@ -194,7 +194,7 @@ class UQBTreeIndex(name: Name.IndexName, parent: DefaultEntity) : AbstractIndex(
             /* Iterate over entity and update index with entries. */
             val cursor = entityTx.cursor(this.columns)
             cursor.forEach { record ->
-                val value = record[this.dbo.columns[0]] ?: throw TxException.TxValidationException(this.context.txId, "Value cannot be null for UniqueHashIndex ${this@UQBTreeIndex.name} given value is (value = null, tupleId = ${record.tupleId}).")
+                val value = record[this.columns[0]] ?: throw TxException.TxValidationException(this.context.txId, "Value cannot be null for UniqueHashIndex ${this@UQBTreeIndex.name} given value is (value = null, tupleId = ${record.tupleId}).")
                 if (!this.addMapping(value, record.tupleId)) {
                     throw TxException.TxValidationException(this.context.txId, "Value must be unique for UniqueHashIndex ${this@UQBTreeIndex.name} but is not (value = $value, tupleId = ${record.tupleId}).")
                 }
