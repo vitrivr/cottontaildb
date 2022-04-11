2ab5348634e34c74a2c4961095d0a14d402bcb41
Ralph Gasser <rg@pontius.ch>
2022-04-11 16:41:24 +0200

    Added additional index- and column-level events.

--- cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/events/IndexEvent.kt
+++ cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/events/IndexEvent.kt
@@ -15,7 +15,17 @@ interface IndexEvent: Event {
     val index: Name.IndexName
 
     /**
-     *
+     * An [IndexEvent] that signals creation of an [Index].
+     */
+    data class Created(override val index: Name.IndexName): IndexEvent
+
+    /**
+     * An [IndexEvent] that signals dropping of an [Index].
+     */
+    data class Dropped(override val index: Name.IndexName): IndexEvent
+
+    /**
+     * An [IndexEvent] that signals [IndexState] change of an [Index].
      */
     data class State(override val index: Name.IndexName, val state: IndexState): IndexEvent
 }
