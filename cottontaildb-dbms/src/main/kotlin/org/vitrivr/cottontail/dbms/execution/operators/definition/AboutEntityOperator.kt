9e3cf0eaf54e4c3ad5da982eff8d65e4d359b445
Ralph Gasser <rg@pontius.ch>
2022-04-25 18:39:42 +0200

    Changed last column produced by AboutEntityOperator to a more general 'info' column.

--- cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/execution/operators/definition/AboutEntityOperator.kt
+++ cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/execution/operators/definition/AboutEntityOperator.kt
@@ -6,13 +6,14 @@ import org.vitrivr.cottontail.core.basics.Record
 import org.vitrivr.cottontail.core.database.ColumnDef
 import org.vitrivr.cottontail.core.database.Name
 import org.vitrivr.cottontail.core.recordset.StandaloneRecord
-import org.vitrivr.cottontail.core.values.BooleanValue
 import org.vitrivr.cottontail.core.values.IntValue
 import org.vitrivr.cottontail.core.values.StringValue
 import org.vitrivr.cottontail.dbms.catalogue.CatalogueTx
+import org.vitrivr.cottontail.dbms.column.ColumnTx
 import org.vitrivr.cottontail.dbms.entity.EntityTx
 import org.vitrivr.cottontail.dbms.execution.operators.basics.Operator
 import org.vitrivr.cottontail.dbms.execution.transactions.TransactionContext
+import org.vitrivr.cottontail.dbms.index.IndexTx
 import org.vitrivr.cottontail.dbms.queries.operators.ColumnSets
 import org.vitrivr.cottontail.dbms.schema.SchemaTx
 
@@ -28,9 +29,7 @@ class AboutEntityOperator(private val tx: CatalogueTx, private val name: Name.En
     override fun toFlow(context: TransactionContext): Flow<Record> = flow {
         val schemaTxn = context.getTx(this@AboutEntityOperator.tx.schemaForName(this@AboutEntityOperator.name.schema())) as SchemaTx
         val entityTxn = context.getTx(schemaTxn.entityForName(this@AboutEntityOperator.name)) as EntityTx
-        val count = entityTxn.count()
         val columns = this@AboutEntityOperator.columns.toTypedArray()
-
         var rowId = 0L
 
         /* Describe entity. */
@@ -38,7 +37,7 @@ class AboutEntityOperator(private val tx: CatalogueTx, private val name: Name.En
             StringValue(this@AboutEntityOperator.name.toString()),
             StringValue("ENTITY"),
             null,
-            IntValue(count),
+            IntValue(entityTxn.count()),
             null,
             null
         )))
@@ -46,13 +45,21 @@ class AboutEntityOperator(private val tx: CatalogueTx, private val name: Name.En
         /* Describe columns. */
         val cols = entityTxn.listColumns()
         cols.forEach {
+            val column = entityTxn.columnForName(it.name)
+            val columnTx = context.getTx(column) as ColumnTx<*>
             emit(StandaloneRecord(rowId++, columns, arrayOf(
                 StringValue(it.name.toString()),
                 StringValue("COLUMN"),
                 StringValue(it.type.name),
-                IntValue(count),
+                IntValue(columnTx.count()),
                 IntValue(it.type.logicalSize),
-                BooleanValue(it.nullable)
+                if (it.primary) {
+                    StringValue("PRIMARY KEY")
+                } else if (it.nullable) {
+                    StringValue("NULLABLE")
+                } else {
+                    StringValue("NOT NULLABLE")
+                }
             )))
         }
 
@@ -60,13 +67,14 @@ class AboutEntityOperator(private val tx: CatalogueTx, private val name: Name.En
         val indexes = entityTxn.listIndexes()
         indexes.forEach {
             val index = entityTxn.indexForName(it)
+            val indexTx = context.getTx(index) as IndexTx
             emit(StandaloneRecord(rowId++, columns, arrayOf(
                 StringValue(it.toString()),
                 StringValue("INDEX"),
                 StringValue(index.type.toString()),
                 null,
                 null,
-                null
+                StringValue(indexTx.state.toString())
             )))
         }
     }
