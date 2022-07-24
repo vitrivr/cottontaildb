1b0256fa25b978c9f976c2007b66c129e6fd28db
Ralph Gasser <rg@pontius.ch>
2022-07-24 11:58:21 +0200

    Added PartitionCalculator method for ColumnTx.

--- cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/execution/operators/sources/PartitionCalculator.kt
+++ cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/execution/operators/sources/PartitionCalculator.kt
@@ -1,5 +1,6 @@
 package org.vitrivr.cottontail.dbms.execution.operators.sources
 
+import org.vitrivr.cottontail.dbms.column.ColumnTx
 import org.vitrivr.cottontail.dbms.entity.EntityTx
 import java.lang.Long.min
 import java.lang.Math.floorDiv
@@ -9,8 +10,28 @@ import java.lang.Math.floorDiv
  *
  * @param partitionIndex The [partitionIndex].
  * @param partitions Total number of partitions.
+ * @return [LongRange] of the partition.
  */
 fun EntityTx.partitionFor(partitionIndex: Int, partitions: Int): LongRange {
+    require(partitionIndex >= 0) { "Partition index must be greater than zero!" }
+    require(partitionIndex < partitions) { "Partition index must be smaller than the total number of partitions!" }
+    val maxTupleId = this.largestTupleId()
+    val minTupleId = this.smallestTupleId()
+    val range = maxTupleId - minTupleId
+    val partitionSize = floorDiv(range, partitions) + 1L
+    val start = minTupleId + partitionIndex * partitionSize
+    val end = min(start + partitionSize - 1, maxTupleId)
+    return start .. end
+}
+
+/**
+ * Obtains a [LongRange] partition for the given [partitionIndex] and the number of [partitions].
+ *
+ * @param partitionIndex The [partitionIndex].
+ * @param partitions Total number of partitions.
+ * @return [LongRange] of the partition.
+ */
+fun ColumnTx<*>.partitionFor(partitionIndex: Int, partitions: Int): LongRange {
     require(partitionIndex >= 0) { "Partition index must be greater than zero!" }
     require(partitionIndex < partitions) { "Partition index must be smaller than the total number of partitions!" }
     val maxTupleId = this.largestTupleId()
