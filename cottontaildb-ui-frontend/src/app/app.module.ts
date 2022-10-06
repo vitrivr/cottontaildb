c72fee8ab6374d2312fc05ce0d13c28380b46f3a
rasmuswilli <rasmus.willi@gmail.com>
2022-10-06 22:41:41 +0200

    add first components and basic layout to frontend

--- cottontaildb-ui-frontend/src/app/app.module.ts
+++ cottontaildb-ui-frontend/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { TabsComponent } from './components/main/tabs/tabs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './components/main/main.component';
+import { MatTabsModule} from "@angular/material/tabs";
+import { SidebarComponent} from "./components/sidebar/sidebar.component";
+import { HeaderComponent } from './components/main/header/header.component';
+import { OverviewComponent } from './components/sidebar/overview/overview.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    SidebarComponent,
+    MainComponent,
+    HeaderComponent,
+    OverviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
+  exports: [
+    TabsComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
