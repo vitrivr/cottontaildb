f74bdfd926bfd87eed7b45db5f2b6e45160030e6
silvanheller <silvan.heller@unibas.ch>
2022-10-10 13:28:18 +0200

    renaming command

--- cottontaildb-cli/src/main/kotlin/org/vitrivr/cottontail/cli/entity/DeleteRowCommand.kt
+++ cottontaildb-cli/src/main/kotlin/org/vitrivr/cottontail/cli/entity/DeleteRowCommand.kt
@@ -8,22 +8,19 @@ import io.grpc.StatusException
 import org.vitrivr.cottontail.cli.AbstractCottontailCommand
 import org.vitrivr.cottontail.client.SimpleClient
 import org.vitrivr.cottontail.client.language.basics.predicate.Expression
-import org.vitrivr.cottontail.client.language.basics.predicate.Predicate
-import org.vitrivr.cottontail.client.language.ddl.DropEntity
 import org.vitrivr.cottontail.client.language.dml.Delete
-import org.vitrivr.cottontail.grpc.CottontailGrpc
 import org.vitrivr.cottontail.utilities.TabulationUtilities
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
 /**
- * Command to drop, i.e., remove a row from an [org.vitrivr.cottontail.dbms.entity.DefaultEntity] by its value in a specific column.
+ * Command to remove a row from an [org.vitrivr.cottontail.dbms.entity.DefaultEntity] by its value in a specific column.
  *
  * @author Silvan Heller
  * @version 1.0.0
  */
 @ExperimentalTime
-class DropRowCommand(client: SimpleClient) : AbstractCottontailCommand.Entity(client, name = "drop-row", help = "Drops all rows where a given column matches a given value from the database. Usage: entity drop-row -c <col> -v <value> <schema>.<entity>") {
+class DeleteRowCommand(client: SimpleClient) : AbstractCottontailCommand.Entity(client, name = "delete-row", help = "Deletes all rows where a given column matches a given value from the database. Usage: entity delete-row -c <col> -v <value> <schema>.<entity>") {
 
     /** Flag that can be used to directly provide confirmation. */
     private val confirm: Boolean by option(
@@ -35,18 +32,18 @@ class DropRowCommand(client: SimpleClient) : AbstractCottontailCommand.Entity(cl
     val value: String by option("-v", "--value", help = "The value").required()
 
     override fun exec() {
-        if (this.confirm || TermUi.confirm("Do you really want to drop the rows from ${this.entityName} where where $col = $value [y/N]?", default = false, showDefault = false) == true) {
+        if (this.confirm || TermUi.confirm("Do you really want to delete the rows from ${this.entityName} where where $col = $value [y/N]?", default = false, showDefault = false) == true) {
             try {
                 val timedTable = measureTimedValue {
                     TabulationUtilities.tabulate(this.client.delete(Delete().from(entityName.fqn).where(Expression(this.col, "=", this.value))))
                 }
-                println("Successfully dropped rows where $col = $value from entity ${this.entityName} (took ${timedTable.duration}).")
+                println("Successfully deleted rows where $col = $value from entity ${this.entityName} (took ${timedTable.duration}).")
                 print(timedTable.value)
             } catch (e: StatusException) {
-                println("Failed to drop rows from entity ${this.entityName} due to error: ${e.message}.")
+                println("Failed to delete rows from entity ${this.entityName} due to error: ${e.message}.")
             }
         } else {
-            println("Drop row aborted.")
+            println("Delete row aborted.")
         }
     }
 }
