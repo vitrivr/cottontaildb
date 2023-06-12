1d292d8a1f23ddd0a35fd79c9a98d6f53332f7a8
Ralph Gasser <ralph.gasser@unibas.ch>
2023-06-12 20:08:18 +0200

    estimateTupleSize() now makes sure that a value can not have a size of zero (or smaller).

--- cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/statistics/StatisticsUtilities.kt
+++ cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/statistics/StatisticsUtilities.kt
@@ -13,8 +13,8 @@ import org.vitrivr.cottontail.dbms.statistics.values.*
  */
 fun Map<ColumnDef<*>, ValueStatistics<*>>.estimateTupleSize() = this.map {
     when (val type = it.key.type) {
-        Types.String -> it.value.avgWidth * Char.SIZE_BYTES
-        Types.ByteString -> it.value.avgWidth
+        Types.String -> it.value.avgWidth.coerceAtLeast(1) * Char.SIZE_BYTES
+        Types.ByteString -> it.value.avgWidth.coerceAtLeast(1)
         else -> type.physicalSize
     }
 }.sum()
