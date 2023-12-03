83dd6743180712983c6dfaf6142b820062b44be8
Ralph Gasser <rg@pontius.ch>
2023-12-03 08:57:46 +0100

    Fixes entity bitmap store name in DefaultEntity / DefualtSchema.

--- cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/execution/transactions/TransactionManager.kt
+++ cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/execution/transactions/TransactionManager.kt
@@ -319,7 +319,9 @@ class TransactionManager(val executionManager: ExecutionManager, val config: Con
                     }
 
                     /* Execute actual commit. */
-                    if (!this@AbstractTransaction.xodusTx.commit()) {
+                    if (this@AbstractTransaction.xodusTx.isIdempotent) {
+                        this@AbstractTransaction.xodusTx.abort()
+                    } else if (!this@AbstractTransaction.xodusTx.commit()) {
                         throw TransactionException.InConflict(this@AbstractTransaction.transactionId)
                     }
                     this@AbstractTransaction.state = TransactionStatus.COMMIT
