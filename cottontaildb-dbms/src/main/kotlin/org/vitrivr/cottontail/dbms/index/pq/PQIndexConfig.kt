7fbb15403d72c2dda45edf19ed71e50ac6278fd8
Ralph Gasser <ralph.gasser@unibas.ch>
2024-01-13 09:34:39 +0100

    Remove COSINE distance as supported function.

--- cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/index/pq/PQIndexConfig.kt
+++ cottontaildb-dbms/src/main/kotlin/org/vitrivr/cottontail/dbms/index/pq/PQIndexConfig.kt
@@ -5,7 +5,6 @@ import jetbrains.exodus.bindings.IntegerBinding
 import jetbrains.exodus.bindings.StringBinding
 import jetbrains.exodus.util.LightOutputStream
 import org.vitrivr.cottontail.core.database.Name
-import org.vitrivr.cottontail.core.queries.functions.math.distance.binary.CosineDistance
 import org.vitrivr.cottontail.core.queries.functions.math.distance.binary.EuclideanDistance
 import org.vitrivr.cottontail.core.queries.functions.math.distance.binary.ManhattanDistance
 import org.vitrivr.cottontail.core.queries.functions.math.distance.binary.SquaredEuclideanDistance
@@ -45,7 +44,7 @@ data class PQIndexConfig(val distance: Name.FunctionName, val numCentroids: Int,
         const val DEFAULT_SUBSPACES = 8
 
         /** Set of supported distances. */
-        val SUPPORTED_DISTANCES: Set<Name.FunctionName> = setOf(ManhattanDistance.FUNCTION_NAME, EuclideanDistance.FUNCTION_NAME, SquaredEuclideanDistance.FUNCTION_NAME, CosineDistance.FUNCTION_NAME)
+        val SUPPORTED_DISTANCES: Set<Name.FunctionName> = setOf(ManhattanDistance.FUNCTION_NAME, EuclideanDistance.FUNCTION_NAME, SquaredEuclideanDistance.FUNCTION_NAME)
     }
 
     /**
