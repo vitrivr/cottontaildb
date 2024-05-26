b50996a23b814ba4ac127c61cc697abb643b92e9
Ralph Gasser <rg@pontius.ch>
2024-05-26 13:51:11 +0200

    StringValueSerializer no longer uses Snappy.

--- cottontaildb-client/src/main/kotlin/org/vitrivr/cottontail/serialization/buffer/StringValueSerializer.kt
+++ cottontaildb-client/src/main/kotlin/org/vitrivr/cottontail/serialization/buffer/StringValueSerializer.kt
@@ -2,8 +2,8 @@ package org.vitrivr.cottontail.serialization.buffer
 
 import org.vitrivr.cottontail.core.types.Types
 import org.vitrivr.cottontail.core.values.StringValue
-import org.xerial.snappy.Snappy
 import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
 
 /**
  * A [ValueSerializer] [StringValue] serialization and deserialization.
@@ -13,6 +13,6 @@ import java.nio.ByteBuffer
  */
 data object StringValueSerializer: ValueSerializer<StringValue> {
     override val type = Types.String
-    override fun fromBuffer(buffer: ByteBuffer) = StringValue(Snappy.uncompressString(buffer.array(), 0, buffer.remaining()))
-    override fun toBuffer(value: StringValue): ByteBuffer = ByteBuffer.wrap(Snappy.compress(value.value))
+    override fun fromBuffer(buffer: ByteBuffer) = StringValue(StandardCharsets.UTF_8.decode(buffer).toString())
+    override fun toBuffer(value: StringValue): ByteBuffer = StandardCharsets.UTF_8.encode(value.value)
 }
