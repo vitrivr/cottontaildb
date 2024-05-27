1f389cae397014098a6803918c490a9dc3179d7d
Ralph Gasser <ralph.gasser@unibas.ch>
2024-05-27 11:05:22 +0200

    Optimized XodusDirectoryTest.

--- cottontaildb-dbms/src/test/kotlin/org/vitrivr/cottontail/storage/lucene/XodusDirectoryTest.kt
+++ cottontaildb-dbms/src/test/kotlin/org/vitrivr/cottontail/storage/lucene/XodusDirectoryTest.kt
@@ -7,26 +7,27 @@ import org.apache.lucene.store.ByteBuffersDirectory
 import org.apache.lucene.store.IOContext
 import org.apache.lucene.store.IndexInput
 import org.apache.lucene.store.IndexOutput
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.vitrivr.cottontail.dbms.AbstractDatabaseTest
+import org.vitrivr.cottontail.test.TestConstants
 import org.vitrivr.cottontail.utilities.io.TxFileUtilities
-import java.nio.file.Paths
 
 /**
  * A series of unit tests for the [XodusDirectory].
  *
  * @author Ralph Gasser
- * @version 1.1.0
+ * @version 1.2.0
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class XodusDirectoryTest {
     /** The random number generator. */
     val random = JDKRandomGenerator()
 
+    /** Temporary directory for tests. */
+    private val path = TestConstants.testConfig().root
+
     /** The Xodus [Environment] used by this [XodusDirectoryTest]. */
-    private lateinit var environment: Environment
+    private val environment = Environments.newInstance(this.path.toFile())
 
     /**
      * Tests ordinary reading and writing from/to [IndexInput] and [IndexOutput]
@@ -35,30 +36,31 @@ class XodusDirectoryTest {
     fun testSingleTxRequestAndRenameFile() {
         val txn = this.environment.beginTransaction()
         val directory = XodusDirectory("test", txn)
+        try {
+            /* Prepare data to write. */
+            val output = directory.createOutput("test", IOContext())
+            val bytes = ByteArray(4096)
+            this.random.nextBytes(bytes)
 
-        /* Prepare data to write. */
-        val output = directory.createOutput("test", IOContext())
-        val bytes = ByteArray(4096)
-        this.random.nextBytes(bytes)
+            /* Write to output . */
+            for (b in bytes) {
+                output.writeByte(b)
+            }
 
-        /* Write to output . */
-        for (b in bytes) {
-            output.writeByte(b)
+            /* Close output. */
+            output.close()
+
+            /* Test file's existence. */
+            Assertions.assertTrue(directory.listAll().contains("test"))
+
+            /* Rename file and test again. */
+            directory.rename("test", "test1")
+            Assertions.assertFalse(directory.listAll().contains("test"))
+            Assertions.assertTrue(directory.listAll().contains("test1"))
+        } finally { /* Commit. */
+            directory.close()
+            txn.commit()
         }
-
-        /* Close output. */
-        output.close()
-
-        /* Test file's existence. */
-        Assertions.assertTrue(directory.listAll().contains("test"))
-
-        /* Rename file and test again. */
-        directory.rename("test", "test1")
-        Assertions.assertFalse(directory.listAll().contains("test"))
-        Assertions.assertTrue(directory.listAll().contains("test1"))
-
-        /* Commit. */
-        txn.commit()
     }
 
     /**
@@ -69,41 +71,45 @@ class XodusDirectoryTest {
         /* Prepare directories. */
         val txn = this.environment.beginTransaction()
         val directory = XodusDirectory("test", txn)
-        val referenceDirectory = ByteBuffersDirectory()
 
-        /* Prepare data to write. */
-        val output = directory.createOutput("test", IOContext())
-        val outputRef = referenceDirectory.createOutput("test", IOContext())
-        val bytes = ByteArray(4096)
-        this.random.nextBytes(bytes)
+        try {
+            val referenceDirectory = ByteBuffersDirectory()
 
-        /* Write to output . */
-        for (b in bytes) {
-            output.writeByte(b)
-            outputRef.writeByte(b)
+            /* Prepare data to write. */
+            val output = directory.createOutput("test", IOContext())
+            val outputRef = referenceDirectory.createOutput("test", IOContext())
+            val bytes = ByteArray(4096)
+            this.random.nextBytes(bytes)
+
+            /* Write to output . */
+            for (b in bytes) {
+                output.writeByte(b)
+                outputRef.writeByte(b)
+            }
+
+            /* Compare the checksums. */
+            Assertions.assertEquals(output.checksum, outputRef.checksum)
+
+            /* Close outputs. */
+            output.close()
+            outputRef.close()
+
+            /* Read data. */
+            val input = directory.openInput("test", IOContext())
+            val inputRef = referenceDirectory.openInput("test", IOContext())
+
+            Assertions.assertEquals(input.length(), inputRef.length())
+            for (i in 0 until input.length()) {
+                Assertions.assertEquals(input.readByte(), inputRef.readByte())
+            }
+
+            /* Close inputs. */
+            input.close()
+            inputRef.close()
+        } finally { /* Commit txn. */
+            directory.close()
+            txn.commit()
         }
-
-        /* Compare the checksums. */
-        Assertions.assertEquals(output.checksum, outputRef.checksum)
-
-        /* Close outputs. */
-        output.close()
-        outputRef.close()
-
-        /* Read data. */
-        val input = directory.openInput("test", IOContext())
-        val inputRef = referenceDirectory.openInput("test", IOContext())
-
-        Assertions.assertEquals(input.length(), inputRef.length())
-        for (i in 0 until input.length()) {
-            Assertions.assertEquals(input.readByte(), inputRef.readByte())
-        }
-
-        /* Close inputs. */
-        input.close()
-        inputRef.close()
-
-        txn.commit()
     }
 
     /**
@@ -114,46 +120,51 @@ class XodusDirectoryTest {
         /* Prepare directories. */
         val txn = this.environment.beginTransaction()
         val directory = XodusDirectory("test", txn)
-        val referenceDirectory = ByteBuffersDirectory()
 
-        /* Prepare data to write. */
-        val output = directory.createOutput("test", IOContext())
-        val outputRef = referenceDirectory.createOutput("test", IOContext())
-        val bytes = ByteArray(4096)
-        this.random.nextBytes(bytes)
+        try {
+            val referenceDirectory = ByteBuffersDirectory()
 
-        /* Write to output . */
-        output.writeBytes(bytes, 0, 1024)
-        output.writeBytes(bytes, 1027, 1)
-        output.writeBytes(bytes, 2048, 1024)
+            /* Prepare data to write. */
+            val output = directory.createOutput("test", IOContext())
+            val outputRef = referenceDirectory.createOutput("test", IOContext())
+            val bytes = ByteArray(4096)
+            this.random.nextBytes(bytes)
 
-        /* Write to reference output. */
-        outputRef.writeBytes(bytes, 0, 1024)
-        outputRef.writeBytes(bytes, 1027, 1)
-        outputRef.writeBytes(bytes, 2048, 1024)
+            /* Write to output . */
+            output.writeBytes(bytes, 0, 1024)
+            output.writeBytes(bytes, 1027, 1)
+            output.writeBytes(bytes, 2048, 1024)
 
-        /* Compare the checksums. */
-        Assertions.assertEquals(output.checksum, outputRef.checksum)
+            /* Write to reference output. */
+            outputRef.writeBytes(bytes, 0, 1024)
+            outputRef.writeBytes(bytes, 1027, 1)
+            outputRef.writeBytes(bytes, 2048, 1024)
 
-        /* Close outputs. */
-        output.close()
-        outputRef.close()
+            /* Compare the checksums. */
+            Assertions.assertEquals(output.checksum, outputRef.checksum)
 
-        /* Read data. */
-        val input = directory.openInput("test", IOContext())
-        val inputRef = referenceDirectory.openInput("test", IOContext())
+            /* Close outputs. */
+            output.close()
+            outputRef.close()
 
-        Assertions.assertEquals(input.length(), inputRef.length())
-        for (i in 0 until input.length()) {
-            Assertions.assertEquals(input.readByte(), inputRef.readByte())
-            Assertions.assertEquals(input.filePointer, inputRef.filePointer)
+            /* Read data. */
+            val input = directory.openInput("test", IOContext())
+            val inputRef = referenceDirectory.openInput("test", IOContext())
+
+            Assertions.assertEquals(input.length(), inputRef.length())
+            for (i in 0 until input.length()) {
+                Assertions.assertEquals(input.readByte(), inputRef.readByte())
+                Assertions.assertEquals(input.filePointer, inputRef.filePointer)
+            }
+
+            /* Close inputs. */
+            input.close()
+            inputRef.close()
+        } finally {
+            /* Commit txn. */
+            directory.close()
+            txn.commit()
         }
-
-        /* Close inputs. */
-        input.close()
-        inputRef.close()
-
-        txn.commit()
     }
 
     /**
@@ -164,59 +175,64 @@ class XodusDirectoryTest {
         /* Prepare directories. */
         val txn = this.environment.beginTransaction()
         val directory = XodusDirectory("test", txn)
-        val referenceDirectory = ByteBuffersDirectory()
 
-        /* Prepare data to write. */
-        val output = directory.createOutput("test", IOContext())
-        val outputRef = referenceDirectory.createOutput("test", IOContext())
-        val bytes = ByteArray(4096)
-        this.random.nextBytes(bytes)
+        try {
+            val referenceDirectory = ByteBuffersDirectory()
 
-        /** Write to output . */
-        output.writeBytes(bytes, 0, 1024)
-        output.writeBytes(bytes, 1027, 1)
-        output.writeBytes(bytes, 2048, 1024)
+            /* Prepare data to write. */
+            val output = directory.createOutput("test", IOContext())
+            val outputRef = referenceDirectory.createOutput("test", IOContext())
+            val bytes = ByteArray(4096)
+            this.random.nextBytes(bytes)
 
-        /** Write to reference output. */
-        outputRef.writeBytes(bytes, 0, 1024)
-        outputRef.writeBytes(bytes, 1027, 1)
-        outputRef.writeBytes(bytes, 2048, 1024)
+            /** Write to output . */
+            output.writeBytes(bytes, 0, 1024)
+            output.writeBytes(bytes, 1027, 1)
+            output.writeBytes(bytes, 2048, 1024)
 
-        /** Compare the checksums. */
-        Assertions.assertEquals(output.checksum, outputRef.checksum)
+            /** Write to reference output. */
+            outputRef.writeBytes(bytes, 0, 1024)
+            outputRef.writeBytes(bytes, 1027, 1)
+            outputRef.writeBytes(bytes, 2048, 1024)
 
-        /** Close outputs. */
-        output.close()
-        outputRef.close()
+            /** Compare the checksums. */
+            Assertions.assertEquals(output.checksum, outputRef.checksum)
 
-        /** Read data. */
-        val input = directory.openInput("test", IOContext())
-        val inputRef = referenceDirectory.openInput("test", IOContext())
+            /** Close outputs. */
+            output.close()
+            outputRef.close()
 
-        /* Create and compare slice. */
-        val sliced = input.slice("test", 1024, 1024)
-        val slicedRef = input.slice("test", 1024, 1024)
+            /** Read data. */
+            val input = directory.openInput("test", IOContext())
+            val inputRef = referenceDirectory.openInput("test", IOContext())
 
-        Assertions.assertEquals(sliced.length(), slicedRef.length())
-        for (i in 0 until sliced.length()) {
-            Assertions.assertEquals(sliced.readByte(), slicedRef.readByte())
-            Assertions.assertEquals(sliced.filePointer, slicedRef.filePointer)
+            /* Create and compare slice. */
+            val sliced = input.slice("test", 1024, 1024)
+            val slicedRef = input.slice("test", 1024, 1024)
+
+            Assertions.assertEquals(sliced.length(), slicedRef.length())
+            for (i in 0 until sliced.length()) {
+                Assertions.assertEquals(sliced.readByte(), slicedRef.readByte())
+                Assertions.assertEquals(sliced.filePointer, slicedRef.filePointer)
+            }
+
+            /* Create and compare slice of slice. */
+            val sliced2 = sliced.slice("test2", 128, 512)
+            val slicedRef2 = sliced.slice("test2", 128, 512)
+            Assertions.assertEquals(sliced2.length(), slicedRef2.length())
+            for (i in 0 until sliced2.length()) {
+                Assertions.assertEquals(sliced2.readByte(), slicedRef2.readByte())
+                Assertions.assertEquals(sliced2.filePointer, slicedRef2.filePointer)
+            }
+
+            /* Close inputs. */
+            input.close()
+            inputRef.close()
+        } finally {
+            /* Commit txn. */
+            directory.close()
+            txn.commit()
         }
-
-        /* Create and compare slice of slice. */
-        val sliced2 = sliced.slice("test2", 128, 512)
-        val slicedRef2 = sliced.slice("test2", 128, 512)
-        Assertions.assertEquals(sliced2.length(), slicedRef2.length())
-        for (i in 0 until sliced2.length()) {
-            Assertions.assertEquals(sliced2.readByte(), slicedRef2.readByte())
-            Assertions.assertEquals(sliced2.filePointer, slicedRef2.filePointer)
-        }
-
-        /** Close inputs. */
-        input.close()
-        inputRef.close()
-
-        txn.commit()
     }
 
     /**
@@ -227,54 +243,58 @@ class XodusDirectoryTest {
         /* Prepare directories. */
         val txn = this.environment.beginTransaction()
         val directory = XodusDirectory("test", txn)
-        val referenceDirectory = ByteBuffersDirectory()
 
-        /* Prepare data to write. */
-        val output = directory.createOutput("test", IOContext())
-        val outputRef = referenceDirectory.createOutput("test", IOContext())
-        val bytes = ByteArray(4096)
-        this.random.nextBytes(bytes)
+        try {
+            val referenceDirectory = ByteBuffersDirectory()
 
-        /** Write to output . */
-        output.writeBytes(bytes, 0, 1024)
-        output.writeBytes(bytes, 1027, 1)
-        output.writeBytes(bytes, 2048, 1024)
+            /* Prepare data to write. */
+            val output = directory.createOutput("test", IOContext())
+            val outputRef = referenceDirectory.createOutput("test", IOContext())
+            val bytes = ByteArray(4096)
+            this.random.nextBytes(bytes)
 
-        /** Write to reference output. */
-        outputRef.writeBytes(bytes, 0, 1024)
-        outputRef.writeBytes(bytes, 1027, 1)
-        outputRef.writeBytes(bytes, 2048, 1024)
+            /** Write to output . */
+            output.writeBytes(bytes, 0, 1024)
+            output.writeBytes(bytes, 1027, 1)
+            output.writeBytes(bytes, 2048, 1024)
 
-        /** Compare the checksums. */
-        Assertions.assertEquals(output.checksum, outputRef.checksum)
+            /** Write to reference output. */
+            outputRef.writeBytes(bytes, 0, 1024)
+            outputRef.writeBytes(bytes, 1027, 1)
+            outputRef.writeBytes(bytes, 2048, 1024)
 
-        /** Close outputs. */
-        output.close()
-        outputRef.close()
+            /** Compare the checksums. */
+            Assertions.assertEquals(output.checksum, outputRef.checksum)
 
-        /** Read data. */
-        val input = directory.openInput("test", IOContext())
-        val inputRef = referenceDirectory.openInput("test", IOContext())
+            /** Close outputs. */
+            output.close()
+            outputRef.close()
 
-        /** Seek position. */
-        val seek = this.random.nextLong(1024L)
-        input.seek(seek)
-        inputRef.seek(seek)
+            /** Read data. */
+            val input = directory.openInput("test", IOContext())
+            val inputRef = referenceDirectory.openInput("test", IOContext())
 
-        /* Compare bytes. */
-        Assertions.assertEquals(input.length(), inputRef.length())
-        for (i in 0 until (input.length() - seek)) {
-            Assertions.assertEquals(input.readByte(), inputRef.readByte())
-            Assertions.assertEquals(input.filePointer, inputRef.filePointer)
+            /** Seek position. */
+            val seek = this.random.nextLong(1024L)
+            input.seek(seek)
+            inputRef.seek(seek)
+
+            /* Compare bytes. */
+            Assertions.assertEquals(input.length(), inputRef.length())
+            for (i in 0 until (input.length() - seek)) {
+                Assertions.assertEquals(input.readByte(), inputRef.readByte())
+                Assertions.assertEquals(input.filePointer, inputRef.filePointer)
+            }
+
+            /* Close inputs. */
+            input.close()
+            inputRef.close()
+            directory.close()
+        } finally {
+            /* Commit txn. */
+            directory.close()
+            txn.commit()
         }
-
-        /** Close inputs. */
-        input.close()
-        inputRef.close()
-        directory.close()
-
-        /** Commit txn. */
-        txn.commit()
     }
 
     /**
@@ -303,14 +323,15 @@ class XodusDirectoryTest {
         outputRef.writeBytes(bytes, 1027, 1)
         outputRef.writeBytes(bytes, 2048, 1024)
 
-        /** Compare the checksums. */
+        /* Compare the checksums. */
         Assertions.assertEquals(output.checksum, outputRef.checksum)
 
-        /** Close outputs. */
+        /* Close outputs. */
         output.close()
         outputRef.close()
 
-        /** Commit transaction. */
+        /* Commit transaction. */
+        directory.close()
         txn1.commit()
 
         /* Open new Txn and XodusDirectory. */
@@ -325,12 +346,12 @@ class XodusDirectoryTest {
             Assertions.assertEquals(input.readByte(), inputRef.readByte())
         }
 
-        /** Close inputs. */
+        /**Close inputs. */
         input.close()
         inputRef.close()
-        directory2.close()
 
         /* Abort second transaction. */
+        directory2.close()
         txn2.abort()
     }
 
@@ -339,7 +360,6 @@ class XodusDirectoryTest {
      */
     @BeforeEach
     fun initialize() {
-        this.environment = Environments.newInstance(Paths.get("testdb").toFile())
     }
 
     /**
@@ -347,9 +367,12 @@ class XodusDirectoryTest {
      */
     @AfterEach
     fun teardown() {
-        this.environment.close()
+        this.environment.clear()
+    }
 
-        /* Delete unnecessary files. */
-        TxFileUtilities.delete(Paths.get(this.environment.location))
+    @AfterAll
+    fun cleanup() {
+        this.environment.close()
+        TxFileUtilities.delete(this.path)
     }
 }
